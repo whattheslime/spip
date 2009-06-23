7ef61eb1246db196309b020c48ad5e0758cbdbe9
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-23 16:29:44 +0000

    un test pour complet pour suivre_liens ...
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@29344 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- filtres/suivre_liens.php
+++ filtres/suivre_liens.php
@@ -7,40 +7,212 @@
 		# source   # lien    # resultat
 $tests = array(
 array(
-	suivre_lien('http://toto/', 'http://tata/'), 'http://tata/'
+	'http://host/', 'http://tata/', 'http://tata/'
 ),
 array(
-	suivre_lien('http://toto/', 'tata'), 'http://toto/tata'
+	'http://host/', '/tata/', 'http://host/tata/'
 ),
 array(
-	suivre_lien('http://toto/ad?hic', '?hoc'), 'http://toto/?hoc'
+	'http://host/', '#tata', 'http://host/#tata'
 ),
 array(
-	suivre_lien('http://toto/./', '#hup'), 'http://toto/#hup'
+	'http://host/', '', 'http://host/'
 ),
 array(
-	suivre_lien('http://toto/fleche/de/tout', '/bois/'), 'http://toto/bois/'
+	'http://host/', 'tata', 'http://host/tata'
 ),
 array(
-	suivre_lien('http://toto/du/lac#1', 'yop'), 'http://toto/du/yop'
+	'http://host/', '?par=value', 'http://host/?par=value'
 ),
 array(
-	suivre_lien('http://toto/', 'http://tata/'), 'http://tata/'
+	'http://host/', 'tata?par=value', 'http://host/tata?par=value'
 ),
 array(
-	suivre_lien('http://toto/allo', '#3'), 'http://toto/allo#3'
+	'http://host/', 'tata#ancre', 'http://host/tata#ancre'
 ),
 array(
-	suivre_lien('http://toto/', 'http://tata/./'), 'http://tata/'
+	'http://host/', 'tata?par=value#ancre', 'http://host/tata?par=value#ancre'
+),
+
+
+
+array(
+	'http://host/page', 'http://tata/', 'http://tata/'
 ),
 array(
-	suivre_lien('http://toto/et#lui', ''), 'http://toto/et#lui'
+	'http://host/page', '/tata/', 'http://host/tata/'
 ),
 array(
-	suivre_lien('http://toto', './'), 'http://toto/'
+	'http://host/page', '#tata', 'http://host/page#tata'
 ),
 array(
-	suivre_lien('http://toto/hop/a', './'), 'http://toto/hop/'
+	'http://host/page', '', 'http://host/page'
+),
+array(
+	'http://host/page', 'tata', 'http://host/tata'
+),
+array(
+	'http://host/page', '?par=value', 'http://host/?par=value'
+),
+array(
+	'http://host/page', 'tata?par=value', 'http://host/tata?par=value'
+),
+array(
+	'http://host/page', 'tata#ancre', 'http://host/tata#ancre'
+),
+array(
+	'http://host/page', 'tata?par=value#ancre', 'http://host/tata?par=value#ancre'
+),
+
+
+array(
+	'http://host/rep/page', 'http://tata/', 'http://tata/'
+),
+array(
+	'http://host/rep/page', '/tata/', 'http://host/tata/'
+),
+array(
+	'http://host/rep/page', '#tata', 'http://host/rep/page#tata'
+),
+array(
+	'http://host/rep/page', '', 'http://host/rep/page'
+),
+array(
+	'http://host/rep/page', 'tata', 'http://host/rep/tata'
+),
+array(
+	'http://host/rep/page', '?par=value', 'http://host/rep/?par=value'
+),
+array(
+	'http://host/rep/page', 'tata?par=value', 'http://host/rep/tata?par=value'
+),
+array(
+	'http://host/rep/page', 'tata#ancre', 'http://host/rep/tata#ancre'
+),
+array(
+	'http://host/rep/page', 'tata?par=value#ancre', 'http://host/rep/tata?par=value#ancre'
+),
+
+
+
+array(
+	'http://host/rep/page#anchor', 'http://tata/', 'http://tata/'
+),
+array(
+	'http://host/rep/page#anchor', '/tata/', 'http://host/tata/'
+),
+array(
+	'http://host/rep/page#anchor', '#tata', 'http://host/rep/page#tata'
+),
+array(
+	'http://host/rep/page#anchor', '', 'http://host/rep/page#anchor'
+),
+array(
+	'http://host/rep/page#anchor', 'tata', 'http://host/rep/tata'
+),
+array(
+	'http://host/rep/page#anchor', '?par=value', 'http://host/rep/?par=value'
+),
+array(
+	'http://host/rep/page#anchor', 'tata?par=value', 'http://host/rep/tata?par=value'
+),
+array(
+	'http://host/rep/page#anchor', 'tata#ancre', 'http://host/rep/tata#ancre'
+),
+array(
+	'http://host/rep/page#anchor', 'tata?par=value#ancre', 'http://host/rep/tata?par=value#ancre'
+),
+
+
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc', 'http://tata/', 'http://tata/'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc', '/tata/', 'http://host/tata/'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc', '#tata', 'http://host/rep/page?titi=valeur&bidule=chose/truc#tata'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc', '', 'http://host/rep/page?titi=valeur&bidule=chose/truc'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc', 'tata', 'http://host/rep/tata'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc', '?par=value', 'http://host/rep/?par=value'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc', 'tata?par=value', 'http://host/rep/tata?par=value'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc', 'tata#ancre', 'http://host/rep/tata#ancre'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc', 'tata?par=value#ancre', 'http://host/rep/tata?par=value#ancre'
+),
+
+
+
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor', 'http://tata/', 'http://tata/'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor', '/tata/', 'http://host/tata/'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor', '#tata', 'http://host/rep/page?titi=valeur&bidule=chose/truc#tata'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor', '', 'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor', 'tata', 'http://host/rep/tata'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor', '?par=value', 'http://host/rep/?par=value'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor', 'tata?par=value', 'http://host/rep/tata?par=value'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor', 'tata#ancre', 'http://host/rep/tata#ancre'
+),
+array(
+	'http://host/rep/page?titi=valeur&bidule=chose/truc#anchor', 'tata?par=value#ancre', 'http://host/rep/tata?par=value#ancre'
+),
+
+
+
+array(
+	'http://toto/ad?hic', '?hoc', 'http://toto/?hoc'
+),
+array(
+	'http://toto/./', '#hup', 'http://toto/#hup'
+),
+array(
+	'http://toto/fleche/de/tout', '/bois/', 'http://toto/bois/'
+),
+array(
+	'http://toto/du/lac#1', 'yop', 'http://toto/du/yop'
+),
+array(
+	'http://toto/', 'http://tata/', 'http://tata/'
+),
+array(
+	'http://toto/allo', '#3', 'http://toto/allo#3'
+),
+array(
+	'http://toto/', 'http://tata/./', 'http://tata/'
+),
+array(
+	'http://toto/et#lui', '', 'http://toto/et#lui'
+),
+array(
+	'http://toto', './', 'http://toto/'
+),
+array(
+	'http://toto/hop/a', './', 'http://toto/hop/'
 )
 );
 
@@ -49,8 +221,13 @@ array(
 	//
 
 	foreach ($tests as $c => $u)
-		if ($u[0] !== $u[1]) {
-			echo "test $c: ".htmlspecialchars($u[0]).' =! '.htmlspecialchars($u[1])."<br />\n";
+		if (($s=suivre_lien($u[0],$u[1])) !== $u[2]) {
+			echo "test $c: suivre_lien("
+			.htmlspecialchars($u[0])
+			.','
+			.htmlspecialchars($u[1])
+			.') = '
+			.htmlspecialchars($s).' mais =! '.htmlspecialchars($u[2])."<br />\n";
 			$err++;
 		}
 
