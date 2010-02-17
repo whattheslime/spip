4b2fb8f9896a9e0b2a87dff055250b53b132d86a
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-17 16:01:06 +0000

    reparer le test sur les inclusions manquantes, pour cause de modification de l'api debusqueur
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@35281 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- balises/inclure_manquant_fonctions.php
+++ balises/inclure_manquant_fonctions.php
@@ -1,7 +1,8 @@
 <?php
 
 	function fin_inclure_manquant() {
-		if (!$GLOBALS['tableau_des_erreurs'])
+		if (!$GLOBALS['tableau_des_erreurs'] 
+			AND !(_request('var_mode') == 'debug' AND erreur_squelette(false)))
 			return "pas d'erreur declenchee";
 
 		$GLOBALS['tableau_des_erreurs'] = array();
