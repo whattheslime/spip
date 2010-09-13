302b6cc3336b8d78026b6ac7c4f82142e2b1c0ca
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-13 10:16:17 +0000

    un test pour explique_config qui analyse le $cfg passe a lire_config et ecrire_config
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@40727 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ config/explique_config.php
@@ -0,0 +1,46 @@
+<?php
+
+	// nom du test
+	$test = 'explique_config';
+
+	// recherche test.inc qui nous ouvre au monde spip
+	$deep = 1;
+	$include = '../tests/test.inc';
+	while (!defined('_SPIP_TEST_INC') && $deep++ < 6) {
+		$include = '../' . $include;
+		@include $include;
+	}
+	if (!defined('_SPIP_TEST_INC')) {
+		die("Pas de $include");
+	}
+
+	include_spip('inc/config');
+
+### explique_config ###
+
+
+	$essais[] = array(array('meta',null,array()), '');
+	$essais[] = array(array('meta','casier',array()), 'casier');
+	$essais[] = array(array('meta','casier',array('sous')), 'casier/sous');
+	$essais[] = array(array('meta','casier',array('sous','plus','bas','encore')), 'casier/sous/plus/bas/encore');
+
+	$essais[] = array(array('meta',null,array()), '/meta');
+	$essais[] = array(array('meta','casier',array()), '/meta/casier');
+	$essais[] = array(array('meta','casier',array('sous')), '/meta/casier/sous');
+	$essais[] = array(array('meta','casier',array('sous','plus','bas','encore')), '/meta/casier/sous/plus/bas/encore');
+
+	$essais[] = array(array('toto',null,array()), '/toto');
+	$essais[] = array(array('toto','casier',array()), '/toto/casier');
+	$essais[] = array(array('toto','casier',array('sous')), '/toto/casier/sous');
+	$essais[] = array(array('toto','casier',array('sous','plus','bas','encore')), '/toto/casier/sous/plus/bas/encore');
+
+	$err = tester_fun('explique_config', $essais);
+	
+	// si le tableau $err est pas vide ca va pas
+	if ($err) {
+		die ('<b>explique_config</b><dl>' . join('', $err) . '</dl>');
+	}
+
+	echo "OK";
+
+?>
