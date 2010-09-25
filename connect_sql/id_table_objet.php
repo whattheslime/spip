c869eb299074082ec968e85c143dab2589ea83c8
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-25 12:16:16 +0000

    un test pour id_table_objet aussi
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@41134 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ connect_sql/id_table_objet.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Test unitaire de la fonction heures
+ * du fichier inc/filtres.php
+ *
+ * genere automatiquement par TestBuilder
+ * le 
+ */
+
+	$test = 'id_table_objet';
+	require '../test.inc';
+	find_in_path("base/connect_sql.php",'',true);
+
+	//
+	// hop ! on y va
+	//
+	$err = tester_fun('id_table_objet', essais_id_table_objet());
+	
+	// si le tableau $err est pas vide ca va pas
+	if ($err) {
+		die ('<dl>' . join('', $err) . '</dl>');
+	}
+
+	echo "OK";
+	
+
+	function essais_id_table_objet(){
+		$essais = array (
+  array (
+    0 => 'id_article',
+    1 => 'articles',
+  ),
+  array (
+    0 => 'id_article',
+    1 => 'article',
+  ),
+  array (
+    0 => 'id_article',
+    1 => 'spip_articles',
+  ),
+  array (
+    0 => 'id_article',
+    1 => 'id_article',
+  ),
+  array (
+    0 => 'id_rubrique',
+    1 => 'rubriques',
+  ),
+  array (
+    0 => 'id_rubrique',
+    1 => 'spip_rubriques',
+  ),
+  array (
+    0 => 'id_rubrique',
+    1 => 'id_rubrique',
+  ),
+  array (
+    0 => 'id_breve',
+    1 => 'breves',
+  ),
+  array (
+    0 => 'id_breve',
+    1 => 'spip_breves',
+  ),
+  array (
+    0 => 'id_breve',
+    1 => 'id_breve',
+  ),
+  array (
+    0 => 'id_mot',
+    1 => 'mots',
+  ),
+  array (
+    0 => 'id_mot',
+    1 => 'spip_mots',
+  ),
+  array (
+    0 => 'id_mot',
+    1 => 'id_mot',
+  ),
+  array (
+    0 => 'id_groupe',
+    1 => 'groupes_mots',
+  ),
+  array (
+    0 => 'id_groupe',
+    1 => 'spip_groupes_mots',
+  ),
+  array (
+    0 => 'id_groupe',
+    1 => 'id_groupe',
+  ),
+  array (
+    0 => 'id_groupe',
+    1 => 'groupes_mot',
+  ),
+  array (
+    0 => 'id_syndic',
+    1 => 'syndic',
+  ),
+  array (
+    0 => 'id_syndic',
+    1 => 'site',
+  ),
+  array (
+    0 => 'id_syndic',
+    1 => 'spip_syndic',
+  ),
+  array (
+    0 => 'id_syndic',
+    1 => 'id_syndic',
+  ),
+  array (
+    0 => 'id_syndic_article',
+    1 => 'syndic_articles',
+  ),
+  array (
+    0 => 'id_syndic_article',
+    1 => 'spip_syndic_articles',
+  ),
+  array (
+    0 => 'id_syndic_article',
+    1 => 'id_syndic_article',
+  ),
+  array (
+    0 => 'id_syndic_article',
+    1 => 'syndic_article',
+  ),
+  array (
+    0 => 'id_article',
+    1 => 'petitions',
+  ),
+  array (
+    0 => 'id_article',
+    1 => 'petition',
+  ),
+  array (
+    0 => 'id_article',
+    1 => 'spip_petitions',
+  ),
+  array (
+    0 => 'id_signature',
+    1 => 'signatures',
+  ),
+  array (
+    0 => 'id_signature',
+    1 => 'signature',
+  ),
+  array (
+    0 => 'id_signature',
+    1 => 'spip_signatures',
+  ),
+  array (
+    0 => 'id_signature',
+    1 => 'id_signature',
+  ),
+
+);
+		return $essais;
+	}
+
+
+
+?>
