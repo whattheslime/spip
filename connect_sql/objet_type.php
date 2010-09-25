f694ee88568c02333eb8e56d540b23d5fe5a581a
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-25 11:11:51 +0000

    testons serieusement ces trois fonctions essentielles table_objet(), table_objet_sql() et objet_type()
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@41127 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ connect_sql/objet_type.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Test unitaire de la fonction heures
+ * du fichier inc/filtres.php
+ *
+ * genere automatiquement par TestBuilder
+ * le 
+ */
+
+	$test = 'objet_type';
+	require '../test.inc';
+	find_in_path("base/connect_sql.php",'',true);
+
+	//
+	// hop ! on y va
+	//
+	$err = tester_fun('objet_type', essais_objet_type());
+	
+	// si le tableau $err est pas vide ca va pas
+	if ($err) {
+		die ('<dl>' . join('', $err) . '</dl>');
+	}
+
+	echo "OK";
+	
+
+	function essais_objet_type(){
+		$essais = array (
+  array (
+    0 => 'article',
+    1 => 'articles',
+  ),
+  array (
+    0 => 'article',
+    1 => 'article',
+  ),
+  array (
+    0 => 'article',
+    1 => 'spip_articles',
+  ),
+  array (
+    0 => 'article',
+    1 => 'id_article',
+  ),
+  array (
+    0 => 'rubrique',
+    1 => 'rubriques',
+  ),
+  array (
+    0 => 'rubrique',
+    1 => 'spip_rubriques',
+  ),
+  array (
+    0 => 'rubrique',
+    1 => 'id_rubrique',
+  ),
+  array (
+    0 => 'breve',
+    1 => 'breves',
+  ),
+  array (
+    0 => 'breve',
+    1 => 'spip_breves',
+  ),
+  array (
+    0 => 'breve',
+    1 => 'id_breve',
+  ),
+  array (
+    0 => 'mot',
+    1 => 'mots',
+  ),
+  array (
+    0 => 'mot',
+    1 => 'spip_mots',
+  ),
+  array (
+    0 => 'mot',
+    1 => 'id_mot',
+  ),
+  array (
+    0 => 'groupe_mots',
+    1 => 'groupes_mots',
+  ),
+  array (
+    0 => 'groupe_mots',
+    1 => 'spip_groupes_mots',
+  ),
+  array (
+    0 => 'groupe_mots',
+    1 => 'id_groupe',
+  ),
+  array (
+    0 => 'groupe_mots',
+    1 => 'groupes_mot',
+  ),
+  array (
+    0 => 'site',
+    1 => 'syndic',
+  ),
+  array (
+    0 => 'site',
+    1 => 'site',
+  ),
+  array (
+    0 => 'site',
+    1 => 'spip_syndic',
+  ),
+  array (
+    0 => 'site',
+    1 => 'id_syndic',
+  ),
+  array (
+    0 => 'syndic_article',
+    1 => 'syndic_articles',
+  ),
+  array (
+    0 => 'syndic_article',
+    1 => 'spip_syndic_articles',
+  ),
+  array (
+    0 => 'syndic_article',
+    1 => 'id_syndic_article',
+  ),
+  array (
+    0 => 'syndic_article',
+    1 => 'syndic_article',
+  ),
+  array (
+    0 => 'petition',
+    1 => 'petitions',
+  ),
+  array (
+    0 => 'petition',
+    1 => 'petition',
+  ),
+  array (
+    0 => 'petition',
+    1 => 'spip_petitions',
+  ),
+  array (
+    0 => 'signature',
+    1 => 'signatures',
+  ),
+  array (
+    0 => 'signature',
+    1 => 'signature',
+  ),
+  array (
+    0 => 'signature',
+    1 => 'spip_signatures',
+  ),
+  array (
+    0 => 'signature',
+    1 => 'id_signature',
+  ),
+
+);
+		return $essais;
+	}
+
+
+
+?>
---
+++ connect_sql/table_objet_sql.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Test unitaire de la fonction heures
+ * du fichier inc/filtres.php
+ *
+ * genere automatiquement par TestBuilder
+ * le 
+ */
+
+	$test = 'table_objet_sql';
+	require '../test.inc';
+	find_in_path("base/connect_sql.php",'',true);
+
+	//
+	// hop ! on y va
+	//
+	$err = tester_fun('table_objet_sql', essais_table_objet_sql());
+	
+	// si le tableau $err est pas vide ca va pas
+	if ($err) {
+		die ('<dl>' . join('', $err) . '</dl>');
+	}
+
+	echo "OK";
+	
+
+	function essais_table_objet_sql(){
+		$essais = array (
+  array (
+    0 => 'spip_articles',
+    1 => 'articles',
+  ),
+  array (
+    0 => 'spip_articles',
+    1 => 'article',
+  ),
+  array (
+    0 => 'spip_articles',
+    1 => 'spip_articles',
+  ),
+  array (
+    0 => 'spip_articles',
+    1 => 'id_article',
+  ),
+  array (
+    0 => 'spip_rubriques',
+    1 => 'rubrique',
+  ),
+  array (
+    0 => 'spip_rubriques',
+    1 => 'spip_rubriques',
+  ),
+  array (
+    0 => 'spip_rubriques',
+    1 => 'id_rubrique',
+  ),
+  array (
+    0 => 'spip_breves',
+    1 => 'breve',
+  ),
+  array (
+    0 => 'spip_breves',
+    1 => 'spip_breve',
+  ),
+  array (
+    0 => 'spip_breves',
+    1 => 'id_breve',
+  ),
+  array (
+    0 => 'spip_mots',
+    1 => 'mot',
+  ),
+  array (
+    0 => 'spip_mots',
+    1 => 'spip_mots',
+  ),
+  array (
+    0 => 'spip_mots',
+    1 => 'id_mot',
+  ),
+  array (
+    0 => 'spip_groupes_mots',
+    1 => 'groupe_mots',
+  ),
+  array (
+    0 => 'spip_groupes_mots',
+    1 => 'spip_groupes_mots',
+  ),
+  array (
+    0 => 'spip_groupes_mots',
+    1 => 'id_groupe',
+  ),
+  array (
+    0 => 'spip_groupes_mots',
+    1 => 'groupes_mot',
+  ),
+  array (
+    0 => 'spip_syndic',
+    1 => 'syndic',
+  ),
+  array (
+    0 => 'spip_syndic',
+    1 => 'site',
+  ),
+  array (
+    0 => 'spip_syndic',
+    1 => 'spip_syndic',
+  ),
+  array (
+    0 => 'spip_syndic',
+    1 => 'id_syndic',
+  ),
+  array (
+    0 => 'spip_syndic_articles',
+    1 => 'syndic_article',
+  ),
+  array (
+    0 => 'spip_syndic_articles',
+    1 => 'spip_syndic_articles',
+  ),
+  array (
+    0 => 'spip_syndic_articles',
+    1 => 'id_syndic_article',
+  ),
+  array (
+    0 => 'spip_syndic_articles',
+    1 => 'syndic_article',
+  ),
+  array (
+    0 => 'spip_petitions',
+    1 => 'petition',
+  ),
+  array (
+    0 => 'spip_petitions',
+    1 => 'petitions',
+  ),
+  array (
+    0 => 'spip_petitions',
+    1 => 'spip_petitions',
+  ),
+  array (
+    0 => 'spip_signatures',
+    1 => 'signature',
+  ),
+  array (
+    0 => 'spip_signatures',
+    1 => 'signatures',
+  ),
+  array (
+    0 => 'spip_signatures',
+    1 => 'spip_signatures',
+  ),
+  array (
+    0 => 'spip_signatures',
+    1 => 'id_signature',
+  ),
+
+);
+		return $essais;
+	}
+
+?>
