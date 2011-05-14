ed484ac66a72de3bce23ea11d4743af62ea99546
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-14 12:46:06 +0000

    mise à jour du test
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@47790 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- filtres_mime/filtre_text_csv_dist.php
+++ filtres_mime/filtre_text_csv_dist.php
@@ -29,29 +29,29 @@
   0 => 
   array (
     0 => '<table class="spip">
-<thead><tr class=\'row_first\'><th scope=\'col\'>A</th><th scope=\'col\'>B</th><th scope=\'col\'>C</th><th scope=\'col\'>D</th><th scope=\'col\'>E</th><th scope=\'col\'>F</th></tr></thead>
+<thead><tr class=\'row_first\'><th id=\'id8549_c0\'>A</th><th id=\'id8549_c1\'>B</th><th id=\'id8549_c2\'>C</th><th id=\'id8549_c3\'>D</th><th id=\'id8549_c4\'>E</th><th id=\'id8549_c5\'>F</th></tr></thead>
 <tbody>
-<tr class=\'row_even\'>
-<td>un</td>
-<td>tableau</td>
-<td>csv</td>
-<td>avec</td>
-<td>des</td>
-<td>valeurs</td></tr>
-<tr class=\'row_odd\'>
-<td>dans chaque</td>
-<td>case</td>
-<td>et aussi une</td>
-<td>case</td>
-<td>avec</td>
-<td>des</td></tr>
-<tr class=\'row_even\'>
-<td>&#34;guillemets&#34;</td>
-<td>est-ce</td>
-<td>que</td>
-<td>ça</td>
-<td>marche&nbsp;?</td>
-<td></td></tr>
+<tr class=\'row_odd odd\'>
+<td headers=\'id8549_c0\'>un</td>
+<td headers=\'id8549_c1\'>tableau</td>
+<td headers=\'id8549_c2\'>csv</td>
+<td headers=\'id8549_c3\'>avec</td>
+<td headers=\'id8549_c4\'>des</td>
+<td headers=\'id8549_c5\'>valeurs</td></tr>
+<tr class=\'row_even even\'>
+<td headers=\'id8549_c0\'>dans chaque</td>
+<td headers=\'id8549_c1\'>case</td>
+<td headers=\'id8549_c2\'>et aussi une</td>
+<td headers=\'id8549_c3\'>case</td>
+<td headers=\'id8549_c4\'>avec</td>
+<td headers=\'id8549_c5\'>des</td></tr>
+<tr class=\'row_odd odd\'>
+<td headers=\'id8549_c0\'>&#34;guillemets&#34;</td>
+<td headers=\'id8549_c1\'>est-ce</td>
+<td headers=\'id8549_c2\'>que</td>
+<td headers=\'id8549_c3\'>ça</td>
+<td headers=\'id8549_c4\'>marche&nbsp;?</td>
+<td headers=\'id8549_c5\'></td></tr>
 </tbody>
 </table>',
     1 => 'A;B;C;D;E;F
@@ -62,29 +62,29 @@ dans chaque;case;et aussi une;case;avec;des
   1 => 
   array (
     0 => '<table class="spip">
-<thead><tr class=\'row_first\'><th scope=\'col\'>A</th><th scope=\'col\'>B</th><th scope=\'col\'>C</th><th scope=\'col\'>D</th><th scope=\'col\'>E</th><th scope=\'col\'>F</th></tr></thead>
+<thead><tr class=\'row_first\'><th id=\'id5b64_c0\'>A</th><th id=\'id5b64_c1\'>B</th><th id=\'id5b64_c2\'>C</th><th id=\'id5b64_c3\'>D</th><th id=\'id5b64_c4\'>E</th><th id=\'id5b64_c5\'>F</th></tr></thead>
 <tbody>
-<tr class=\'row_even\'>
-<td>un</td>
-<td>tableau</td>
-<td>csv</td>
-<td>avec</td>
-<td>des</td>
-<td>valeurs</td></tr>
-<tr class=\'row_odd\'>
-<td>dans chaque</td>
-<td>case</td>
-<td>et aussi une</td>
-<td>case</td>
-<td>avec</td>
-<td>des</td></tr>
-<tr class=\'row_even\'>
-<td>guillemets</td>
-<td>est-ce</td>
-<td>que</td>
-<td>ça</td>
-<td>marche&nbsp;?</td>
-<td></td></tr>
+<tr class=\'row_odd odd\'>
+<td headers=\'id5b64_c0\'>un</td>
+<td headers=\'id5b64_c1\'>tableau</td>
+<td headers=\'id5b64_c2\'>csv</td>
+<td headers=\'id5b64_c3\'>avec</td>
+<td headers=\'id5b64_c4\'>des</td>
+<td headers=\'id5b64_c5\'>valeurs</td></tr>
+<tr class=\'row_even even\'>
+<td headers=\'id5b64_c0\'>dans chaque</td>
+<td headers=\'id5b64_c1\'>case</td>
+<td headers=\'id5b64_c2\'>et aussi une</td>
+<td headers=\'id5b64_c3\'>case</td>
+<td headers=\'id5b64_c4\'>avec</td>
+<td headers=\'id5b64_c5\'>des</td></tr>
+<tr class=\'row_odd odd\'>
+<td headers=\'id5b64_c0\'>guillemets</td>
+<td headers=\'id5b64_c1\'>est-ce</td>
+<td headers=\'id5b64_c2\'>que</td>
+<td headers=\'id5b64_c3\'>ça</td>
+<td headers=\'id5b64_c4\'>marche&nbsp;?</td>
+<td headers=\'id5b64_c5\'></td></tr>
 </tbody>
 </table>',
     1 => 'A;B;C;D;E;F
@@ -95,29 +95,29 @@ guillemets;est-ce;que;ça;marche ?;',
   2 => 
   array (
     0 => '<table class="spip">
-<thead><tr class=\'row_first\'><th scope=\'col\'>A</th><th scope=\'col\'>B</th><th scope=\'col\'>C</th><th scope=\'col\'>D</th><th scope=\'col\'>E</th><th scope=\'col\'>F</th></tr></thead>
+<thead><tr class=\'row_first\'><th id=\'idcc66_c0\'>A</th><th id=\'idcc66_c1\'>B</th><th id=\'idcc66_c2\'>C</th><th id=\'idcc66_c3\'>D</th><th id=\'idcc66_c4\'>E</th><th id=\'idcc66_c5\'>F</th></tr></thead>
 <tbody>
-<tr class=\'row_even\'>
-<td>un</td>
-<td>tableau</td>
-<td>csv</td>
-<td>avec</td>
-<td>des</td>
-<td>valeurs</td></tr>
-<tr class=\'row_odd\'>
-<td>dans chaque</td>
-<td>case</td>
-<td>et aussi une</td>
-<td>case</td>
-<td>avec</td>
-<td>des</td></tr>
-<tr class=\'row_even\'>
-<td>&#34;guillemets&#34;</td>
-<td>est-ce</td>
-<td>que</td>
-<td>√ßa</td>
-<td>marche&nbsp;?</td>
-<td></td></tr>
+<tr class=\'row_odd odd\'>
+<td headers=\'idcc66_c0\'>un</td>
+<td headers=\'idcc66_c1\'>tableau</td>
+<td headers=\'idcc66_c2\'>csv</td>
+<td headers=\'idcc66_c3\'>avec</td>
+<td headers=\'idcc66_c4\'>des</td>
+<td headers=\'idcc66_c5\'>valeurs</td></tr>
+<tr class=\'row_even even\'>
+<td headers=\'idcc66_c0\'>dans chaque</td>
+<td headers=\'idcc66_c1\'>case</td>
+<td headers=\'idcc66_c2\'>et aussi une</td>
+<td headers=\'idcc66_c3\'>case</td>
+<td headers=\'idcc66_c4\'>avec</td>
+<td headers=\'idcc66_c5\'>des</td></tr>
+<tr class=\'row_odd odd\'>
+<td headers=\'idcc66_c0\'>&#34;guillemets&#34;</td>
+<td headers=\'idcc66_c1\'>est-ce</td>
+<td headers=\'idcc66_c2\'>que</td>
+<td headers=\'idcc66_c3\'>√ßa</td>
+<td headers=\'idcc66_c4\'>marche&nbsp;?</td>
+<td headers=\'idcc66_c5\'></td></tr>
 </tbody>
 </table>',
     1 => '"A","B","C","D","E","F"
