d64596c7f7c355e00c57885e016fb24b5581d694
BennyB <ben.spip@gmail.com>
2011-09-22 19:44:32 +0000

    for i in 18512 18504 18501 18496 18494 18480 18473 18368 ; do echo 18368; echo -------------; svn merge --change -18368 .; done

--- ecrire/inc/filtres.php
+++ ecrire/inc/filtres.php
@@ -1142,7 +1142,7 @@ function date_fin_semaine($annee, $mois, $jour) {
 // http://doc.spip.org/@agenda_connu
 function agenda_connu($type)
 {
-  return in_array($type, array('jour','mois','semaine','periode', 'trimestre')) ? ' ' : '';
+  return in_array($type, array('jour','mois','semaine','periode')) ? ' ' : '';
 }
 
 
@@ -1219,9 +1219,7 @@ function agenda_periode($type, $nb, $avec, $sans='')
 		$evt = array($sans, $avec, $min, $max);
 		$type = 'mois';
 	}
-	$ancre = _request('ancre');
-	$s =  self('&') . (preg_match('/^[\w-]+$/', $ancre) ? "#$ancre" : '');
-	return http_calendrier_init($start, $type,  _request('echelle'), _request('partie_cal'), $s, $evt);
+	return http_calendrier_init($start, $type,  _request('echelle'), _request('partie_cal'), self('&'), $evt);
 }
 
 
@@ -1232,7 +1230,7 @@ function agenda_controle($date='date', $jour='jour', $mois='mois', $annee='annee
 	$jour = _request($jour);
 	$mois = _request($mois);
 	$annee = _request($annee);
-
+	
 	if (!($jour||$mois||$anne)) {
 		if ($date = recup_date(_request($date))) {
 			list($annee, $mois, $jour ) = $date;
