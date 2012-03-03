863fead300f9832e8288f9eb617a5b35cbb4068d
b_b <brunobergot@gmail.com>
2012-03-03 11:29:58 +0000

    report de 19052 sans oups : un admin ne doit pas pouvoir modifier un webmestre s'il ne l'est pas lui même

--- ecrire/inc/autoriser.php
+++ ecrire/inc/autoriser.php
@@ -573,8 +573,8 @@ function autoriser_auteur_modifier_dist($faire, $type, $id, $qui, $opt) {
 	// ou si les webmestres sont fixes par constante (securite)
 	elseif ($opt['webmestre'] AND (defined('_ID_WEBMESTRES') OR !autoriser('webmestre')))
 		return false;
-	// et toucher au statut d'un webmestre si il ne l'est pas lui meme
-	elseif ($opt['statut'] AND autoriser('webmestre','',0,$id) AND !autoriser('webmestre'))
+	// et modifier un webmestre si il ne l'est pas lui meme
+	elseif (autoriser('webmestre','',0,$id) AND !autoriser('webmestre'))
 		return false;
 	else
 		return true;
