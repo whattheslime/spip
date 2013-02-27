f64197e970ca14a599f0f22660b903de01c378ab
amemo <amemo@rezo.net>
2013-02-27 22:23:49 +0000

    Documentation du code : - En-tête de logout.php par Matthieu Marcillaud
    Dernier test j'espère \! Avec de beaux accents :)

--- ecrire/action/logout.php
+++ ecrire/action/logout.php
@@ -10,6 +10,12 @@
  *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
 \***************************************************************************/
 
+/**
+ * Action pour déconnecter une personne authentifiée
+ *
+ * @package SPIP\Core\Authentification
+ */
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 include_spip('inc/cookie');
