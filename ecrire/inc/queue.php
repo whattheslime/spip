b88f1117ea6b4e92a0e9a9e14e8db29c10d5bc8b
Cerdic <cedric@yterium.com>
2013-03-12 18:30:28 +0000

    Report de r20160 : mauvaise idée de relancer un cron automatiquement immediatement sans tenir compte du retour car cela provoque un appel continu toutes les secondes du genie optimiser qui fait plein de choses en BDD (load issue)

--- ecrire/inc/queue.php
+++ ecrire/inc/queue.php
@@ -362,7 +362,7 @@ function queue_close_job(&$row,$time,$result=0){
 		include_spip('inc/genie');
 		if ($result<0)
 			// relancer tout de suite, mais en baissant la priorite
-			queue_genie_replan_job($row['fonction'],$periode,$time,time(),$row['priorite']-1);
+			queue_genie_replan_job($row['fonction'],$periode,0-$result,null,$row['priorite']-1);
 		else
 			// relancer avec la periode prevue
 			queue_genie_replan_job($row['fonction'],$periode,$time);
