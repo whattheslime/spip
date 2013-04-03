859b992ddb1ba26dfc58292282f93923d9c41bdc
denisb <denisb@a-working-class-hero-is-something-to.be>
2013-04-03 09:39:35 +0000

    debusquer.php : cosmétique (apparition du 'toogle-bug') ; r20397 cassait la navigation du var_profile : on répare.

--- ecrire/public/debusquer.php
+++ ecrire/public/debusquer.php
@@ -138,7 +138,7 @@ function debusquer_compose_message($msg){
 function debusquer_bandeau($erreurs){
 
 	if (!empty($erreurs)){
-		$n = count($erreurs) . ' ' . _T('zbug_erreur_squelette');
+		$n = array(count($erreurs) . ' ' . _T('zbug_erreur_squelette'));
 		return debusquer_navigation($erreurs, $n);
 	}
 	elseif (!empty($GLOBALS['tableau_des_temps'])) {
@@ -170,7 +170,7 @@ function debusquer_contexte($env){
 // Affichage du tableau des erreurs ou des temps de calcul
 // Cliquer sur les numeros en premiere colonne permet de voir le code
 
-function debusquer_navigation($tableau, $caption = '', $id = 'debug-nav'){
+function debusquer_navigation($tableau, $caption = array(), $id = 'debug-nav'){
 
 	if (_request('exec')=='valider_xml') return '';
 	$GLOBALS['bouton_admin_debug'] = true;
@@ -212,10 +212,12 @@ function debusquer_navigation($tableau, $caption = '', $id = 'debug-nav'){
 
 	return "\n<table id='$id'>"
 		. "<caption onclick=\"jQuery(this).parent().toggle();\">"
-		. $caption
+		. $caption[0]
 ## aide locale courte a ecrire, avec lien vers une grosse page de documentation
 #		aide('erreur_compilation'),
 		. "</caption>"
+		//  fausse caption du chrono (mais vraie nav)
+		. (!empty($caption[1]) ? $caption[1] : '')
 		. "<tr><th>"
 		. _T('numero')
 		. "</th><th>"
@@ -456,7 +458,7 @@ function debusquer_squelette($fonc, $mode, $self){
 		}
 		else
 			return strlen(trim($res))
-			? "<div id='spip-debug'>$res</div>"
+			? "<img src='".chemin_image('compat-16.png')."' alt='afficher-masquer le debug' id='spip-debug-toogle' onclick=\"jQuery('#spip-debug').toggle();\" /><div id='spip-debug'>$res</div>"
 			// cas de l'appel sur erreur: montre la page
 			: $GLOBALS['debug_objets']['resultat']['tout'];
 	}
@@ -476,7 +478,7 @@ function debusquer_squelette($fonc, $mode, $self){
 		$res = $id = '';
 	}
 	return !trim($texte) ? '' : (
-		"<div id='spip-debug'>$res"
+		"<img src='".chemin_image('compat-16.png')."' alt='afficher-masquer le debug' id='spip-debug-toogle' onclick=\"jQuery('#spip-debug').toggle();\" /><div id='spip-debug'>$res"
 			. "<div id='debug_boucle'><fieldset$id><legend>"
 			. "<a href='".$self."#f_".substr($fonc, 0, 37)."'> &#8593; "
 			. ($legend ? $legend : $mode)
@@ -738,10 +740,7 @@ function debusquer_entete($titre, $corps){
 		. "' type='text/css' />" .
 		"</head>\n" .
 		"<body style='margin:0 10px;'>\n" .
-		"<div id='spip-debug-hide' style='position: absolute; top: 0px; z-index: 1000; right:0px;'>" .
-		"<a href='#' onClick=\"x = document.getElementById('spip-debug-header'); (x.style.display == '' ? x.style.display = 'none' : x.style.display = ''); return false;\"> + / - </a>" .
-		"</div>" .
-		"<div id='spip-debug-header' style='position: absolute; top: 22px; z-index: 1000;height:97%;left:0px;right:10px;'>" .
+		"<div id='spip-debug-header'>" .
 		$corps .
 		inclure_balise_dynamique(balise_FORMULAIRE_ADMIN_dyn('spip-admin-float', $debug_objets), false) .
 		'</div></body></html>';
