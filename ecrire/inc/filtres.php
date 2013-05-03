aab7c3b08ce96464ee456350592ea7d21ea84d57
Cerdic <cedric@yterium.com>
2013-05-03 08:48:23 +0000

    Report de r20468 : Optimiser #PRODUIRE :- on ne reecrit le fichier que quand il change effectivement en passant par un fichier intermediaire .last (evite le renouvellement inutile des timestamp, qui produisent des feuilles compilees inutilement)- quand on reecrit le fichier il faut appeler clearstatcache() sur ce fichier, sinon PHP ressert le vieux timestamp jusqu'a la fin du hit, et il faut recharger 2 fois la page pour avoir la mise a jour(realet,ne0futur)

--- ecrire/inc/filtres.php
+++ ecrire/inc/filtres.php
@@ -3833,10 +3833,12 @@ function produire_fond_statique($fond, $contexte=array(), $options = array(), $c
 
 	// mettre a jour le fichier si il n'existe pas
 	// ou trop ancien
+	// le dernier fichier produit est toujours suffixe par .last
+	// et recopie sur le fichier cible uniquement si il change
 	if (!file_exists($filename)
-		OR (isset($cache['lastmodified']) AND $cache['lastmodified'] AND filemtime($filename)<$cache['lastmodified'])
-		OR (defined('_VAR_MODE') AND _VAR_MODE=='recalcul')) 
-	{
+		OR !file_exists($filename.".last")
+		OR (isset($cache['lastmodified']) AND $cache['lastmodified'] AND filemtime($filename.".last")<$cache['lastmodified'])
+		OR (defined('_VAR_MODE') AND _VAR_MODE=='recalcul')) {
 		$contenu = $cache['texte'];
 		// passer les urls en absolu si c'est une css
 		if ($extension=="css")
@@ -3847,10 +3849,18 @@ function produire_fond_statique($fond, $contexte=array(), $options = array(), $c
 			$comment = "/* #PRODUIRE{fond=$fond";
 			foreach($contexte as $k=>$v)
 				$comment .= ",$k=$v";
-			$comment .="} le ".date("Y-m-d H:i:s")." */\n";
+			// pas de date dans le commentaire car sinon ca invalide le md5 et force la maj
+			// mais on peut mettre un md5 du contenu, ce qui donne un aperu rapide si la feuille a change ou non
+			$comment .="}\n   md5:".md5($contenu)." */\n";
 		}
 		// et ecrire le fichier
-		ecrire_fichier($filename,$comment.$contenu);
+		ecrire_fichier($filename.".last",$comment.$contenu);
+		// regarder si on recopie
+		if (!file_exists($filename)
+		  OR md5_file($filename)!==md5_file($filename.".last")){
+			@copy($filename.".last",$filename);
+			clearstatcache(true,$filename); // eviter que PHP ne reserve le vieux timestamp
+		}
 	}
 	
 	return $filename;
