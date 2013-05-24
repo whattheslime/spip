fc711e712c6b06df0c78a81d4d7136664451d22f
Cerdic <cedric@yterium.com>
2013-05-24 13:37:28 +0000

    suite à r20552 #FORMULAIRE_INSCRIPTION sans argument utilisé dans la page sommaire de squelettes-dist n'était plus fonctionnel. On rétablit le meilleur choix possible (1comite ou 6forum) au niveau de la fonction de la balise dans ce cas particulier (et on enleve la valeur par defaut sur la page identifiants car elle n'est pas equivalente)

--- ecrire/balise/formulaire_inscription.php
+++ ecrire/balise/formulaire_inscription.php
@@ -31,8 +31,17 @@ function balise_FORMULAIRE_INSCRIPTION ($p) {
 // http://doc.spip.org/@balise_FORMULAIRE_INSCRIPTION_stat
 function balise_FORMULAIRE_INSCRIPTION_stat($args, $context_compil) {
 	list($mode, $focus, $id) = $args;
+
+	if (!$mode){
+		if ($GLOBALS['meta']["accepter_inscriptions"] == "oui")
+			$mode = "1comite";
+		elseif (($GLOBALS['meta']['accepter_visiteurs'] == 'oui' OR $GLOBALS['meta']['forums_publics'] == 'abo')){
+			$mode = "6forum";
+		}
+	}
+
 	include_spip('inc/autoriser');
-	return autoriser('inscrireauteur', $mode, $id) ? $args : false;
+	return autoriser('inscrireauteur', $mode, $id) ? array($mode, $focus, $id) : false;
 }
 
 ?>
