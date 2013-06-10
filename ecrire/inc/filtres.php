8e4872ee4a29ffe9ccf09d6b33f9e5c79fc5cd92
Cerdic <cedric@yterium.com>
2013-06-10 20:27:50 +0000

    Report de r20640 : un filtre liens_nofollow qui ajoute un rel="nofollow" à tous les liens du texte passé en argument

--- ecrire/inc/filtres.php
+++ ecrire/inc/filtres.php
@@ -784,6 +784,29 @@ function liens_ouvrants ($texte) {
 }
 
 /**
+ * Ajouter un attribut rel="nofollow" sur tous les liens d'un texte
+ * @param string $texte
+ * @return string
+ */
+function liens_nofollow($texte) {
+	if (stripos($texte,"<a")===false)
+		return $texte;
+
+	if (preg_match_all(",<a\b[^>]*>,UimsS",$texte, $regs, PREG_PATTERN_ORDER)){
+		foreach($regs[0] as $a){
+			$rel = extraire_attribut($a,"rel");
+			if (strpos($rel,"nofollow")===false){
+				$rel = "nofollow" . ($rel?" $rel":"");
+				$anofollow = inserer_attribut($a,"rel",$rel);
+				$texte = str_replace($a,$anofollow,$texte);
+			}
+		}
+	}
+
+	return $texte;
+}
+
+/**
  * Transforme les sauts de paragraphe HTML `p` en simples passages à la ligne `br`
  *
  * @filtre PtoBR
