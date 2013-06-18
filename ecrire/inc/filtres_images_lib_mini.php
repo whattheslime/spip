3f338d1f3bdfa1c13ed070ed3f29d3876f3c93b0
davux <da@weeno.net>
2013-06-18 12:47:50 +0000

    L'histoire amusante de imagepalettetotruecolor()
    La fonction imagepalettetotruecolor() a été introduite par ARNO* avec
    la révision r8317 en janvier 2007 (et améliorée par la suite).
    
    Six ans et demi après, une fonction du même nom est introduite dans
    PHP 5.5. Par chance, elle a exactement le même comportement que la
    nôtre. La seule différence c'est qu'elle renvoie un booleen alors que
    nous on ne renvoie rien, donc dans ce sens ça ne pose pas de problème.
    
    On ajoute donc simplement une condition sur function_exists(). De cette
    manière c'est notre fonction qui sera toujours utilisée sur les versions
    de PHP actuelles, puis la fonction native sera utilisée sur les sites
    tournant avec PHP 5.5 et suivantes. (Ferme #3016.)
    
    Il suffira ensuite de supprimer notre fonction dans quelques années quand
    la version minimum de PHP pour faire tourner SPIP sera 5.5 ou supérieure.

--- ecrire/inc/filtres_images_lib_mini.php
+++ ecrire/inc/filtres_images_lib_mini.php
@@ -623,7 +623,9 @@ function image_graver($img){
 }
 
 // Transforme une image a palette indexee (256 couleurs max) en "vraies" couleurs RGB
+// Existe seulement pour compatibilite avec PHP < 5.5
 // http://doc.spip.org/@imagepalettetotruecolor
+if (!function_exists("imagepalettetotruecolor")) {
  function imagepalettetotruecolor(&$img) {
 	if ($img AND !imageistruecolor($img) AND function_exists('imagecreatetruecolor')) {
 		$w = imagesx($img);
@@ -641,6 +643,7 @@ function image_graver($img){
 
 		$img = $img1;
 	}
+ }
 }
 
 // http://doc.spip.org/@image_tag_changer_taille
