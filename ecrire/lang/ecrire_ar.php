0f4ae5ce747877ff712d66de4d79e9a1ae6f1c81
Salvatore <salvatore@rezo.net>
2013-07-10 03:36:57 +0000

    langues (ecrire)

--- ecrire/lang/ecrire_ar.php
+++ ecrire/lang/ecrire_ar.php
@@ -819,8 +819,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'sans_heure' => 'بدون توقيت الساعات',
-	'statut_admin_restreint' => ')إدارة محدودة(', # MODIF
-	'statut_webmestre' => 'webmestre', # NEW
+	'statut_admin_restreint' => 'إدارة محدودة',
+	'statut_webmestre' => 'المشرف على الموقع',
 
 	// T
 	'tache_cron_asap' => 'مهمة CRON @function@ (في أسرع وقت)',
