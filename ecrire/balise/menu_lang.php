724a6e3e8be51f47068fd80945c68eb9aec40246
amemo <amemo@rezo.net>
2013-08-23 13:43:55 +0000

    Documentation du code (petites corrections) : - balise_MENU_LANG par Ybbet - filtre_url_reponse_forum par Ybbet

--- ecrire/balise/menu_lang.php
+++ ecrire/balise/menu_lang.php
@@ -26,7 +26,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * et présélectionne celle la globale `$lang`
  * ou de l'arguemnt fourni: `#MENU_LANG{#ENV{malangue}}`
  * 
- * @balise MENU_LANG_ECRIRE
+ * @balise MENU_LANG
  * @link http://www.spip.net/4626
  * 
  * @param Champ $p
