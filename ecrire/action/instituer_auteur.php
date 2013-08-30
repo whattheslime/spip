ea088760bfb29ad8b5c8c05207cd6b3c2f731a38
Matthieu Marcillaud <marcimat@rezo.net>
2013-08-30 07:11:28 +0000

    L'action instituer auteur doit transmettre un tableau des paramètres, pas un tableau de tableau. (Pat)

--- ecrire/action/instituer_auteur.php
+++ ecrire/action/instituer_auteur.php
@@ -27,7 +27,7 @@ function action_instituer_auteur_dist() {
 	include_spip('action/editer_auteur');
 
 	$c = array(
-		array('statut'=>$statut,'id_parent'=>intval(_request('id_parent')),'restreintes'=>_request('restreintes'))
+		'statut'=>$statut,'id_parent'=>intval(_request('id_parent')),'restreintes'=>_request('restreintes')
 	);
 	instituer_auteur($id_auteur, $c);
 
