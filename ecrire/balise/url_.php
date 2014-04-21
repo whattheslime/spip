8231e45b2a8a837c9ad2f03fc8cca601d23e0502
Committo,Ergo:sum <esj@rezo.net>
2014-04-21 13:39:05 +0000

    Contrairement à ce qu'il affirmait, r14473 n'évacuait pas les entités XML dans les #URL_ avec étoile, mais provoquait une erreur de compilation.

--- ecrire/balise/url_.php
+++ ecrire/balise/url_.php
@@ -80,8 +80,8 @@ function balise_URL__dist($p) {
 			$code = generer_generer_url($nom, $p);
 			if ($code === NULL) return NULL;
 		}
-		if (!$p->etoile)
-			$p->code = "vider_url($code)";
+		if ($p->etoile) $code = "str_replace('&amp;', '&', $code)";
+		$p->code = "vider_url($code)";
 		$p->interdire_scripts = false;
 		return $p;
 	}
