08a32bbf7c22ad40a06defff5e1f7d1d39530658
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-09 17:50:41 +0000

    un test unitaire pour http://core.spip.org/issues/2855 : il semble que contrairement à ce que dit le ticket, le markup généré est tout bon sauf que les < et > sont conservés tels quels au lieu d'être convertis en &lt; et gt; Faut-il vraiment fixer ça ?
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@84110 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ unit/propre/chevron_ouvrant.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Test unitaire de la fonction propre
+ * du fichier inc/texte.php
+ *
+ * genere automatiquement par testbuilder
+ * le 
+ */
+
+	$test = 'propre';
+	$remonte = "../";
+	while (!is_dir($remonte."ecrire"))
+		$remonte = "../$remonte";
+	require $remonte.'tests/test.inc';
+	find_in_path("inc/texte.php",'',true);
+	$GLOBALS['meta']['type_urls'] = $type_urls = "page";
+
+	// initialiser les plugins qui changent les intertitre (Z), et les restaurer juste apres
+	$mem = array(
+		isset($GLOBALS['debut_intertitre']) ? $GLOBALS['debut_intertitre'] : null,
+		isset($GLOBALS['spip_raccourcis_typo']) ? $GLOBALS['spip_raccourcis_typo'] : null
+	);
+	propre('rien du tout');
+	list($GLOBALS['debut_intertitre'],$GLOBALS['spip_raccourcis_typo']) = $mem;
+
+	//
+	// hop ! on y va
+	//
+	$err = tester_fun('propre', essais_chevron_ouvrant());
+
+	// si le tableau $err est pas vide ca va pas
+	if ($err) {
+		die ('<dl>' . join('', $err) . '</dl>');
+	}
+
+	echo "OK";
+	
+
+function essais_chevron_ouvrant(){
+		$essais = array (
+  0 => 
+	  array (
+	    0 => '<p>a&lt;b</p>',
+	    1 => 'a<b',
+	  ),
+	1 =>
+	  array (
+	    0 => '<p><i>a&lt;b</i></p>',
+	    1 => '{a<b}',
+	  ),
+	2 =>
+	  array (
+	    0 => '<p><strong>a&lt;b</strong></p>',
+	    1 => '{{a<b}}',
+	  ),
+	3 =>
+	  array (
+	    0 => '<h3 class="spip">a&lt;b</h3>',
+	    1 => '{{{a<b}}}',
+	  ),
+	4 =>
+	  array (
+	    0 => '<p><i>0 &lt; a &lt; 1</i> et <i>a &gt; 5</i></p>',
+	    1 => '{0 < a < 1} et {a > 5}',
+	  ),
+	5 =>
+	  array (
+	    0 => '<p><i>0 &lt; a &lt; 1.0</i> et <i>a &lt; 5</i></p>',
+	    1 => '{0 < a < 1.0} et {a > 5}',
+	  ),
+
+	);
+	return $essais;
+}
