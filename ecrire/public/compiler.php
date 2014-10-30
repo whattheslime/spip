fd5411411371358e5493d958f22cbab586b49ae2
Cerdic <cedric@yterium.com>
2014-10-30 18:30:36 +0000

    Fix #3327 : Respecter les CACA et les PROUT de Rastapopoulos dans la pagination (dont le modele peut être dynamique) (ie argumenter_inclure ignorait tous les arguments dynamiques qui suivaient un argument sans partie "x=", comme #INCLURE{#ENV{X,inclure/pied},id_article} qui est une inclusion directe d'un fichier, dont le contenu brut du fichier #ENV{X,inclure/pied} est importé, le reste du contexte ne servant pas + un modificateur m manquant sur la regexp pour ne pas ingorer les valeurs multilignes )

--- ecrire/public/compiler.php
+++ ecrire/public/compiler.php
@@ -72,10 +72,11 @@ function argumenter_inclure($params, $rejet_filtres, $p, &$boucles, $id_boucle, 
 				$erreur_p_i_i = array('zbug_parametres_inclus_incorrects',
 					 array('param' => $var->nom_champ));
 				erreur_squelette($erreur_p_i_i, $p);
-			  } else $l[1] = calculer_liste($val, $p->descr, $boucles, $id_boucle);
-			  break;
+				  break;
+			  }
+			  else $l[1] = calculer_liste($val, $p->descr, $boucles, $id_boucle);
 			} else {
-				preg_match(",^([^=]*)(=?)(.*)$,", $var->texte,$m);
+				preg_match(",^([^=]*)(=?)(.*)$,m", $var->texte,$m);
 				$m = array_pad($m, 3, null);
 				$var = $m[1];
 				$auto = false;;
