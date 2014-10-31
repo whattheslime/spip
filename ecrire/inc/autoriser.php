af70c63dcf8e1cb66e32be5f5976ce8b91ed03ec
Cerdic <cedric@yterium.com>
2014-10-31 08:22:09 +0000

    Report de r21539 : Fix #2013 : le filtrage /auteur du statut de previsu introduit par r21500 n'etait pas pris en compte dans autoriser_previsualiser_dist() ce qui ne rendait plus visible le lien de prévisu sur les articles en cours de redaction.

--- ecrire/inc/autoriser.php
+++ ecrire/inc/autoriser.php
@@ -345,7 +345,12 @@ function test_previsualiser_objet_champ($type=null, $opt=array()) {
 				$champ = $c['champ'];
 				if (!isset($opt[$champ])) return false; // pas de champ passe a la demande => NIET
 				$previsu = explode(',',$c['previsu']);
-				if (!in_array($opt[$champ],$previsu)) // le statut n'est pas dans ceux definis par la previsu => NIET
+				// regarder si ce statut est autoriser pour l'auteur
+				if (in_array($opt[$champ]."/auteur",$previsu)){
+					if (!sql_countsel("spip_auteurs_liens","id_auteur=".intval($qui['id_auteur'])." AND objet=".sql_quote($type)." AND id_objet=".intval($id)))
+						return false;  // pas auteur de cet objet => NIET
+				}
+				elseif (!in_array($opt[$champ],$previsu)) // le statut n'est pas dans ceux definis par la previsu => NIET
 					return false;
 			}
 		}
