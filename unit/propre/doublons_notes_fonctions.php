f39ffbcaca9f5517d1706d66c2ff65202b18ca80
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-15 16:19:27 +0000

    Notices PHP en moins et bien créer un document dans un article en sqlite
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@86092 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- unit/propre/doublons_notes_fonctions.php
+++ unit/propre/doublons_notes_fonctions.php
@@ -8,7 +8,7 @@ function creer_article_a_doublons_notes() {
 	$res = spip_query("SELECT id_document from spip_documents order by rand() limit 1");
 	if ($a = sql_fetch($res)) {
 		list($doc) = array_values($a);
-		spip_query("REPLACE spip_articles (id_article, titre, statut, texte) VALUES (-1, 'test pour doublons_notes.html', 'prepa', 'hello [[ xx <doc$doc> ]].')");
+		sql_query("REPLACE INTO spip_articles (id_article, titre, statut, texte) VALUES (-1, 'test pour doublons_notes.html', 'prepa', 'hello [[ xx <doc$doc> ]].')");
 	}
 	else
 		die ('NA il faut un document');
