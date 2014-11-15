b360bbc5f82120d798021eea272477f039fb9492
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-15 20:36:07 +0000

    Les tests de hiérarchie étaient presque juste : mais si l'on met {tout} sur la rubrique parente, cela sélectionne aussi les rubriques non publiées, et dans ce cas, la hiérarchie n'est pas bien prise en compte. On sépare en 2 test, avec et sans {tout} sur les rubriques, avec {statut==.*} sur la hiérarchier si {tout} sur la rubrique parente.
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@86101 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- unit/simpletest/dist/boucles/hierarchie.php
+++ unit/simpletest/dist/boucles/hierarchie.php
@@ -2,10 +2,9 @@
 require_once('lanceur_spip.php');
 
 class Test_boucle_hierarchie extends SpipTest{
-	
 	function testBoucleHierarchie(){
 		$this->assertOkCode("
-			<BOUCLE_a(RUBRIQUES){tout}{0,50}>
+			<BOUCLE_a(RUBRIQUES){0,50}>
 			<BOUCLE_secteur(HIERARCHIE){0,1}>[(#ID_RUBRIQUE|=={#_a:ID_SECTEUR}|?{'',
 				erreur secteur: attendu #_a:ID_SECTEUR ; resultat #ID_RUBRIQUE<br />})]
 			</BOUCLE_secteur>
@@ -13,9 +12,22 @@ class Test_boucle_hierarchie extends SpipTest{
 				erreur parent: attendu #_a:ID_PARENT ; resultat #ID_RUBRIQUE<br />})]
 			</BOUCLE_parent>
 			</BOUCLE_a>
-			OK		
+			OK
 		");
-	}	
+	}
+	function testBoucleHierarchieTout(){
+		$this->assertOkCode("
+			<BOUCLE_a(RUBRIQUES){tout}{0,50}>
+			<BOUCLE_secteur(HIERARCHIE){statut==.*}{0,1}>[(#ID_RUBRIQUE|=={#_a:ID_SECTEUR}|?{'',
+				erreur secteur: attendu #_a:ID_SECTEUR ; resultat #ID_RUBRIQUE<br />})]
+			</BOUCLE_secteur>
+			<BOUCLE_parent(HIERARCHIE){statut==.*}{n-1,1}>[(#ID_RUBRIQUE|=={#_a:ID_PARENT}|?{'',
+				erreur parent: attendu #_a:ID_PARENT ; resultat #ID_RUBRIQUE<br />})]
+			</BOUCLE_parent>
+			</BOUCLE_a>
+			OK
+		");
+	}
 }
 
 ?>
