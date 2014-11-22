a0f775b9d7d38da22362a9980c3ba3b97b07abbd
Matthieu Marcillaud <marcimat@rezo.net>
2014-11-22 13:47:48 +0000

    Notice php en moins lorsqu'une boucle correspond à une table sans clé primaire (dans mon test une vue SQL)

--- ecrire/public/compiler.php
+++ ecrire/public/compiler.php
@@ -1185,7 +1185,7 @@ function compiler_squelette($squelette, $boucles, $nom, $descr, $sourcefile, $co
 				if ($show) {
 					$boucles[$id]->show = $show;
 					// recopie les infos les plus importantes
-					$boucles[$id]->primary = $show['key']["PRIMARY KEY"];
+					$boucles[$id]->primary = isset($show['key']["PRIMARY KEY"]) ? $show['key']["PRIMARY KEY"] : '';
 					$boucles[$id]->id_table = $x = preg_replace(",^spip_,","",$show['id_table']);
 					$boucles[$id]->from[$x] = $nom_table = $show['table'];
 					$boucles[$id]->iterateur = 'SQL';
