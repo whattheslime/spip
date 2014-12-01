386fbd896c5e54dfd1721e8a0132633eac9dc1e2
Cerdic <cedric@yterium.com>
2014-12-01 15:44:33 +0000

    date, donc

--- config/ecran_securite.php
+++ config/ecran_securite.php
@@ -5,7 +5,7 @@
  * ------------------
  */
 
-define('_ECRAN_SECURITE', '1.2.2'); // 2014-12-01// 2014-12-01date
+define('_ECRAN_SECURITE', '1.2.2'); // 2014-12-01
 
 /*
  * Documentation : http://www.spip.net/fr_article4200.html
