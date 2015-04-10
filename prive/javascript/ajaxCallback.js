386cbb38ef4a1d9158360d7573cadaf15644998d
b_b <brunobergot@gmail.com>
2015-04-10 12:15:20 +0000

    Ferme #3420 : spécifier l'unité des valeurs pour left et top appliqués au p inséré par ajaxcallback en fin de page (chankalan)

--- prive/javascript/ajaxCallback.js
+++ prive/javascript/ajaxCallback.js
@@ -183,7 +183,7 @@ jQuery.fn.positionner = function(force, setfocus) {
 jQuery.spip.virtualbuffer_id='spip_virtualbufferupdate';
 jQuery.spip.initReaderBuffer = function(){
 	if (jQuery('#'+jQuery.spip.virtualbuffer_id).length) return;
-	jQuery('body').append('<p style="float:left;width:0;height:0;position:absolute;left:-5000;top:-5000;"><input type="hidden" name="'+jQuery.spip.virtualbuffer_id+'" id="'+jQuery.spip.virtualbuffer_id+'" value="0" /></p>');
+	jQuery('body').append('<p style="float:left;width:0;height:0;position:absolute;left:-5000px;top:-5000px;"><input type="hidden" name="'+jQuery.spip.virtualbuffer_id+'" id="'+jQuery.spip.virtualbuffer_id+'" value="0" /></p>');
 }
 jQuery.spip.updateReaderBuffer = function(){
 	var i = jQuery('#'+jQuery.spip.virtualbuffer_id);
