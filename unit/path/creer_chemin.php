2f9bba307dfe042779e14f42f113b07c2e8edf98
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-08 14:04:07 +0000

    Réparer le test de creer_chemin en absence de globale dossier_squelettes de définie.
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@89150 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- unit/path/creer_chemin.php
+++ unit/path/creer_chemin.php
@@ -7,14 +7,26 @@
 	require $remonte.'tests/test.inc';
 
 	$n = count(explode(":",$GLOBALS['dossier_squelettes']));
-	
+
 	$chemin = creer_chemin();
 	_chemin('toto');
 	$chemin1 = creer_chemin();
-	
-	if ((count($chemin1)!=(count($chemin)+1))
-	OR ('toto/'!==$chemin1[$n] AND 'toto/'!==$chemin1[$n+1]))
-		die('Erreur ajout chemin par la fonction _chemin()'.var_dump($chemin).var_dump($chemin1));
+
+	if (count($chemin1) != (count($chemin)+1)) {
+		die('Erreur ajout chemin par la fonction _chemin() : mauvais compte'.var_dump($chemin).var_dump($chemin1));
+	}
+
+	if ($GLOBALS['dossier_squelettes']) {
+		// toto a été ajouté après les chemins de dossier squelettes
+		if ('toto/'!==$chemin1[$n] AND 'toto/'!==$chemin1[$n+1]) {
+			die('Erreur ajout chemin par la fonction _chemin() : avec dossier_squelettes'.var_dump($chemin).var_dump($chemin1));
+		}
+	} else {
+		// toto a été ajouté au tout début (dossier squelettes étant vide)
+		if ('toto/'!==$chemin1[0]) {
+			die('Erreur ajout chemin par la fonction _chemin() : sans dossier_squelettes'.var_dump($chemin).var_dump($chemin1));
+		}
+	}
 
 	$GLOBALS['dossier_squelettes']= "titi:".$GLOBALS['dossier_squelettes'];
 	$chemin2 = creer_chemin();
