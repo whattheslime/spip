b25f3563d1d6a84571edbaad679a154f52089b12
Gilles Vincent <gilles.vincent@gmail.com>
2015-05-23 20:27:46 +0000

    Nettoyage de code : en est en php>4.3

--- ecrire/inc/charger_php_extension.php
+++ ecrire/inc/charger_php_extension.php
@@ -68,15 +68,8 @@ function inc_charger_php_extension_dist($module){
 		return false;
 	}
 
-	/* Once we require PHP >= 4.3, we might use PHP_SHLIB_SUFFIX here */
-	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-		$module_file = 'php_' . $module . '.dll';
-	} elseif (PHP_OS=='HP-UX') {
-		$module_file = $module . '.sl';
-	} else {
-		$module_file = $module . '.so';
-	}
-
-	return @dl($module_file);
+	$prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
+	
+	return @dl($prefix . $module_file . PHP_SHLIB_SUFFIX);
 }
 ?>
