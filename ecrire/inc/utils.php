e564b9840c19a05ca6052b66e5aee01f8c1a00a5
Cerdic <cedric@yterium.com>
2015-05-24 17:31:26 +0000

    retour sur c17521 : var_export plante sur des tableaux recursifs alors que print_r est plus utile pour le debug (b_b via http://php.net/manual/en/function.var-export.php) On utilise cette derniere

--- ecrire/inc/utils.php
+++ ecrire/inc/utils.php
@@ -283,7 +283,7 @@ function spip_log($message=NULL, $name=NULL) {
 				_LOG_DEBUG=>'debug:');
 			$log = charger_fonction('log', 'inc');
 		}
-		if (!is_string($message)) $message = var_export($message, true);
+		if (!is_string($message)) $message = print_r($message, true);
 		$log($pre[$niveau].' '.$message, $logname);
 	}
 }
