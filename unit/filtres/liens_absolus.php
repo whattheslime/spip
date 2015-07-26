02493605db599b4b795917aa168d90267d7bc1a1
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-26 14:37:30 +0000

    liens_absolus passe maintenant par inserer_attribut qui produit des &#38; au lieu de &amp;
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@91080 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- unit/filtres/liens_absolus.php
+++ unit/filtres/liens_absolus.php
@@ -8,11 +8,11 @@
 	include_spip('inc/filtres');
 
 	$essais["lien prive"] =
-	 array("bla bla <a href='".generer_url_ecrire('toto','truc=machin&chose=bidule',false,false)."'>lien prive</a>",
+	 array("bla bla <a href='".str_replace("&amp;","&#38;",generer_url_ecrire('toto','truc=machin&chose=bidule',false,false))."'>lien prive</a>",
 	 "bla bla <a href='".generer_url_ecrire('toto','truc=machin&chose=bidule',false,true)."'>lien prive</a>");
 
 	$essais["lien public"] =
-	 array("bla bla <a href='".generer_url_public('toto','truc=machin&chose=bidule',false,false)."'>lien public</a>",
+	 array("bla bla <a href='".str_replace("&amp;","&#38;",generer_url_public('toto','truc=machin&chose=bidule',false,false))."'>lien public</a>",
 	 "bla bla <a href='".generer_url_public('toto','truc=machin&chose=bidule',false,true)."'>lien public</a>");
 
 	$essais["mailto"] =
