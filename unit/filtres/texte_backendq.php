d5b5fb846f7eac805e39f20e112e53b5724e8c18
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-26 14:36:27 +0000

    le lien initial est inchange par texte_backend
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@91079 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- unit/filtres/texte_backendq.php
+++ unit/filtres/texte_backendq.php
@@ -41,7 +41,7 @@
   ),
   2 => 
   array (
-    0 => 'Un texte avec des &lt;a href=&#034;http://spip.net/&#034;&gt;liens&lt;/a&gt; [Article 1-&gt;art1] [spip-&gt;http://www.spip.net] http://www.spip.net',
+    0 => 'Un texte avec des &lt;a href=&#034;http://spip.net&#034;&gt;liens&lt;/a&gt; [Article 1-&gt;art1] [spip-&gt;http://www.spip.net] http://www.spip.net',
     1 => 'Un texte avec des <a href="http://spip.net">liens</a> [Article 1->art1] [spip->http://www.spip.net] http://www.spip.net',
   ),
   3 => 
