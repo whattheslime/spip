3746aab016d5aadf49f01f25ba6a21881e361959
Cerdic <cedric@yterium.com>
2015-11-03 10:16:46 +0000

    Correction de r22429 : on le leve le flag espace_prive que dans l'espace prive (fil)

--- ecrire/inc/texte.php
+++ ecrire/inc/texte.php
@@ -214,7 +214,7 @@ function typo($letexte, $echapper=true, $connect=null, $env=array()) {
 	if (is_null($connect)){
 		$connect = '';
 		$interdire_script = true;
-		$env['espace_prive'] = 1;
+		$env['espace_prive'] = test_espace_prive();
 	}
 
 	// Echapper les codes <html> etc
