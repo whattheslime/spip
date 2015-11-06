c0ea0421132a33dd443e5bf946a431f21256eeb4
olly.olga <olly.olga@gmail.com>
2015-11-06 04:32:29 +0000

    [Salvatore] [source:spip/ecrire/lang/ public] Export depuis http://trad.spip.net de la langue ru

--- ecrire/lang/public_ru.php
+++ ecrire/lang/public_ru.php
@@ -22,7 +22,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'auteur' => 'Автор',
 	'autres' => 'Прочие',
 	'autres_breves' => 'Другие новости',
-	'autres_groupes_mots_clefs' => 'Другие группы ключевых слов',
+	'autres_groupes_mots_clefs' => 'Другие группы ключей',
 	'autres_sites' => 'Другие сайты',
 
 	// B
@@ -67,9 +67,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'message' => 'Сообщение',
 	'messages_forum' => 'Сообщения форума',
 	'messages_recents' => 'Новые сообщения форума',
-	'mots_clef' => 'Ключевое слово',
-	'mots_clefs' => 'Ключевые слова',
-	'mots_clefs_meme_groupe' => 'Ключевые слова из этой группы',
+	'mots_clef' => 'Ключ',
+	'mots_clefs' => 'Ключи',
+	'mots_clefs_meme_groupe' => 'Ключи из этой группы',
 
 	// N
 	'navigation' => 'Просмотр',
@@ -83,7 +83,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'page_precedente' => 'предыдущая страница',
 	'page_suivante' => 'следующая страница',
 	'par_auteur' => ' ',
-	'participer_site' => 'Вы можете принять активное участие в жизни этого сайта и писать ваши собственные статьи. Все, что надо сделать - зарегистрироваться.',
+	'participer_site' => 'Вы можете принять активное участие в жизни этого сайта и писать ваши собственные статьи. Все, что надо сделать, - зарегистрироваться.',
 	'plan_site' => 'Карта сайта',
 	'popularite' => 'Популярность',
 	'poster_message' => 'Отправить сообщение',
