d5f8894c82c394d6204d475f0dc95ece9853fd0c
george <george@middleeastwatch.net>
2015-11-08 04:32:45 +0000

    [Salvatore] [source:spip/ecrire/lang/ spip] Export depuis http://trad.spip.net de la langue en

--- ecrire/lang/spip_en.php
+++ ecrire/lang/spip_en.php
@@ -597,7 +597,7 @@ and log in to the site.
 	'pass_nouveau_pass' => 'New password',
 	'pass_ok' => 'OK',
 	'pass_oubli_mot' => 'Forgotten password',
-	'pass_procedure_changer' => 'In order to change your password, we have to check your identity first. Please enter the e-mail address associated with this account.',
+	'pass_procedure_changer' => 'In order to change your password, we have to check your identity first. Please enter the e-mail address associated with this account.', # MODIF
 	'pass_quitter_fenetre' => 'Close this window',
 	'pass_rappel_login' => 'Reminder: your identifier (login) is "@login@".',
 	'pass_recevoir_mail' => 'You will receive an email explaining how you can retrieve your access to the site.',
