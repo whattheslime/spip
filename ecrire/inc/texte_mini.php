bf88d5b3b918e3adc2cca1caed7c00425eb62fe4
Fil <fil@rezo.net>
2015-12-02 08:29:48 +0000

    oups, j'ai fait une boulette

--- ecrire/inc/texte_mini.php
+++ ecrire/inc/texte_mini.php
@@ -414,9 +414,7 @@ function echapper_html_suspect($texte){
 	// on teste sur strlen car safehtml supprime le contenu dangereux
 	// mais il peut aussi changer des ' en " sur les attributs html,
 	// donc un test d'egalite est trop strict
-	$sblob = preg_replace(',\W,', '', safehtml($texte));
-	$blob  = preg_replace(',\W,', '', $texte);
-	if ($sblob!==$blob){
+	if (strlen(safehtml($texte))!==strlen($texte)){
 		$texte = str_replace("<","&lt;",$texte);
 	}
 
