3a51d7f2a3c82b8959b0df27f447059ea24b14e4
Cerdic <cedric@yterium.com>
2016-03-10 11:09:31 +0000

    Report de r22899 : Mise a jour de la liste des bots : Moreover|FunWebProducts|facebook|hootsuite|MetaURI|flipboard sont tous des bots

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -138,10 +138,12 @@ if (!defined('_IS_BOT')) {
 	define('_IS_BOT',
 		isset($_SERVER['HTTP_USER_AGENT'])
 		and preg_match(
-		// mots generiques
+			// mots generiques
 			',bot|slurp|crawler|spider|webvac|yandex|'
+			// MSIE 6.0 est un botnet 99,9% du temps, on traite donc ce USER_AGENT comme un bot
+			. 'MSIE 6\.0|'
 			// UA plus cibles
-			. '80legs|accoona|AltaVista|ASPSeek|Baidu|Charlotte|EC2LinkFinder|eStyle|Google|Genieo|INA dlweb|InfegyAtlas|Java VM|LiteFinder|Lycos|Rambler|Scooter|ScrubbyBloglines|Yahoo|Yeti'
+			. '80legs|accoona|AltaVista|ASPSeek|Baidu|Charlotte|EC2LinkFinder|eStyle|facebook|flipboard|hootsuite|FunWebProducts|Google|Genieo|INA dlweb|InfegyAtlas|Java VM|LiteFinder|Lycos|MetaURI|Moreover|Rambler|Scooter|ScrubbyBloglines|Yahoo|Yeti'
 			. ',i', (string)$_SERVER['HTTP_USER_AGENT'])
 	);
 }
