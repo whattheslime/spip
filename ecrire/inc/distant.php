ffa738f72343aa82c22bcf1d0e0e2702142646bb
Cerdic <cedric@yterium.com>
2016-06-25 18:12:13 +0000

    Report de r23057 : recuperer_infos_distantes() renvoyait a tort un tableau avec pour seul argument un mime_type vide quand le document n'existait pas, pouvant laisser croire a son existence. On sort explicitement avec false dans ce cas, comme prevu. Et on essaye meme pas de recuperer l'URL si la source fournie n'en est pas une

--- ecrire/inc/distant.php
+++ ecrire/inc/distant.php
@@ -506,6 +506,11 @@ function fichier_copie_locale($source){
 // http://doc.spip.org/@recuperer_infos_distantes
 function recuperer_infos_distantes($source, $max = 0, $charger_si_petite_image = true){
 
+	// pas la peine de perdre son temps
+	if (!tester_url_absolue($source)) {
+		return false;
+	}
+	
 	# charger les alias des types mime
 	include_spip('base/typedoc');
 	global $mime_alias;
@@ -582,6 +587,11 @@ function recuperer_infos_distantes($source, $max = 0, $charger_si_petite_image =
 	if (!$a AND !$max){
 		spip_log("tenter GET $source");
 		$a = recuperer_infos_distantes($source, _INC_DISTANT_MAX_SIZE);
+	}
+
+	// si on a rien trouve pas la peine d'insister
+	if (!$a) {
+		return false;
 	}
 
 	// S'il s'agit d'une image pas trop grosse ou d'un fichier html, on va aller
