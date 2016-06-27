cd54b3a14c790f8798a20aaf092136646ddf4c51
Cerdic <cedric@yterium.com>
2016-06-27 10:09:31 +0000

    Report de r23066 : passer entites_html sur les id ajax par precaution (Guillaume Fahrer)

--- ecrire/inc/filtres.php
+++ ecrire/inc/filtres.php
@@ -4803,7 +4803,9 @@ function encoder_contexte_ajax($c, $form = '', $emboite = null, $ajaxid = '') {
 	$r = ' data-origin="' . $r . '"';
 	$class = 'ajaxbloc';
 	if ($ajaxid and is_string($ajaxid)) {
-		$class .= ' ajax-id-' . $ajaxid;
+		// ajaxid est normalement conforme a un nom de classe css
+		// on ne verifie pas la conformite, mais on passe entites_html par dessus par precaution
+		$class .= ' ajax-id-' . entites_html($ajaxid);
 	}
 
 	return "<div class='$class' " . "data-ajax-env='$env'$r>\n$emboite</div><!--ajaxbloc-->\n";
