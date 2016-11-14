15db5c243b3893b24e00887544031e19fdb09ad0
kent1 <kent1@arscenic.info>
2016-11-14 16:27:36 +0000

    PSR SPIP

--- prive/informer_auteur_fonctions.php
+++ prive/informer_auteur_fonctions.php
@@ -17,20 +17,20 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 /**
  * Retrouve pour le formulaire de login les informations d'un login qui permettront de crypter le mot de passe saisi
- * 
+ *
  * Si le login n'est pas trouvé, retourne de fausses informations,
  * sauf si la constante `_AUTORISER_AUTH_FAIBLE` est déclarée à true.
- * 
- * @note 
+ *
+ * @note
  *     Le parametre var_login n'est pas dans le contexte pour optimiser le cache
  *     il faut aller le chercher à la main
- * 
+ *
  * @uses auth_informer_login()
  * @uses json_export()
- * 
- * @param string $bof 
- *     Date de la demande 
- * @return string 
+ *
+ * @param string $bof
+ *     Date de la demande
+ * @return string
  *     JSON des différentes informations
  */
 function informer_auteur($bof) {
@@ -41,17 +41,15 @@ function informer_auteur($bof) {
 	$row = auth_informer_login($login);
 	if ($row and is_array($row)) {
 		unset($row['id_auteur']);
-	}
-	// permettre d'autoriser l'envoi de password non crypte lorsque
-	// l'auteur n'est pas (encore) declare dans SPIP, par exemple pour les cas
-	// de premiere authentification via SPIP a une autre application.
-	else {
+	} else {
+		// permettre d'autoriser l'envoi de password non crypte lorsque
+		// l'auteur n'est pas (encore) declare dans SPIP, par exemple pour les cas
+		// de premiere authentification via SPIP a une autre application.
 		if (defined('_AUTORISER_AUTH_FAIBLE') and _AUTORISER_AUTH_FAIBLE) {
 			$row = array();
-		}
-		// generer de fausses infos, mais credibles, pour eviter une attaque
-		// http://core.spip.org/issues/1758
-		else {
+		} else {
+			// generer de fausses infos, mais credibles, pour eviter une attaque
+			// http://core.spip.org/issues/1758
 			include_spip('inc/securiser_action');
 			$fauxalea1 = md5('fauxalea' . secret_du_site() . $login . floor(date('U') / 86400));
 			$fauxalea2 = md5('fauxalea' . secret_du_site() . $login . ceil(date('U') / 86400));
@@ -59,7 +57,7 @@ function informer_auteur($bof) {
 			$row = array(
 				'login' => $login,
 				'cnx' => '0',
-				'logo' => "",
+				'logo' => '',
 				'alea_actuel' => substr_replace($fauxalea1, '.', 24, 0),
 				'alea_futur' => substr_replace($fauxalea2, '.', 24, 0)
 			);
