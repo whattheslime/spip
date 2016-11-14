67b7806b5d0d9b6385c0ea1e6838995fbf492912
kent1 <kent1@arscenic.info>
2016-11-14 16:28:16 +0000

    simples quotes

--- prive/rss/a_suivre_fonctions.php
+++ prive/rss/a_suivre_fonctions.php
@@ -5,8 +5,8 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 function trier_rss($texte) {
-	if (preg_match_all(",<item.*</item>\s*?,Uims", $texte, $matches, PREG_SET_ORDER)) {
-		$placeholder = "<!--REINSERT-->";
+	if (preg_match_all(',<item.*</item>\s*?,Uims', $texte, $matches, PREG_SET_ORDER)) {
+		$placeholder = '<!--REINSERT-->';
 		$items = array();
 		foreach ($matches as $match) {
 			if (preg_match(',<dc:date>(.*)</dc:date>,Uims', $match[0], $r)) {
