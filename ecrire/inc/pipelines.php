80768df9b502e7219c6323ef20aef0fdf3f59fc1
Matthieu Marcillaud <marcimat@rezo.net>
2017-01-18 13:22:34 +0000

    Compatibilité PHP 7.1 : minipipe() utilise call_user_func() qui ne peut pas transmettre de variable par référence. Une notice est maintenant générée en PHP 7.1.
    "call_user_func() and call_user_func_array() will now always throw errors when trying to call a method whose signature has references. Previously this sometimes worked if call_user_func() was used outside a namespace."

--- ecrire/inc/pipelines.php
+++ ecrire/inc/pipelines.php
@@ -252,8 +252,7 @@ function f_recuperer_fond($flux) {
  * @param string $texte Contenu de la page envoyée au navigateur
  * @return string         Contenu de la page envoyée au navigateur
  */
-function f_queue(&$texte) {
-
+function f_queue($texte) {
 	// eviter une inclusion si rien a faire
 	if (_request('action') == 'cron'
 		or queue_sleep_time_to_next_job() > 0
