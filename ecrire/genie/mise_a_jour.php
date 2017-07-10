b3d0259f8ebd903ccc14cde48e0ed887662f8888
b_b <brunobergot@gmail.com>
2017-07-10 17:15:56 +0000

    https pour ce lien aussi (francky)
    je Sesse, tu Sesses, nous Sessons les urls...

--- ecrire/genie/mise_a_jour.php
+++ ecrire/genie/mise_a_jour.php
@@ -153,7 +153,7 @@ function info_maj($dir, $file, $version) {
 	$message = $page ? _T('nouvelle_version_spip', array('version' => $page)) . ($page_majeure ? ' | ' : '') : '';
 	$message .= $page_majeure ? _T('nouvelle_version_spip_majeure', array('version' => $page_majeure)) : '';
 
-	return "<a class='info_maj_spip' href='http://www.spip.net/fr_update' title='$page'>" . $message . '</a>';
+	return "<a class='info_maj_spip' href='https://www.spip.net/fr_update' title='$page'>" . $message . '</a>';
 }
 
 /**
