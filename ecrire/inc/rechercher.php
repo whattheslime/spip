2e4cb2f606730c3d32f04ebaa98b6455a9bc4685
b_b <brunobergot@gmail.com>
2017-09-16 15:49:31 +0000

    Fix #3997 : report de r21716 en 3.0

--- ecrire/inc/rechercher.php
+++ ecrire/inc/rechercher.php
@@ -62,12 +62,14 @@ function expression_recherche($recherche, $options) {
 	// (meme si utilisee dans plusieurs boucles)
 	static $expression = array();
 	$key = serialize(array($recherche, $options['preg_flags']));
-	if (isset($expression[$key]))
+	if (isset($expression[$key])) {
 		return $expression[$key];
+	}
 
 	$u = $GLOBALS['meta']['pcre_u'];
-	if ($u AND strpos($options['preg_flags'],$u)===false)
+	if ($u and strpos($options['preg_flags'], $u) === false) {
 		$options['preg_flags'] .= $u;
+	}
 	include_spip('inc/charsets');
 	$recherche = trim($recherche);
 
@@ -75,69 +77,90 @@ function expression_recherche($recherche, $options) {
 	if (substr($recherche, 0, 1) == '/' and substr($recherche, -1, 1) == '/' and strlen($recherche) > 2) {
 		// c'est une preg
 		$recherche_trans = translitteration($recherche);
-		$preg = $recherche_trans.$options['preg_flags'];
+		$preg = $recherche_trans . $options['preg_flags'];
 		$is_preg = true;
-	}
-	else{
-		// s'il y a plusieurs mots il faut les chercher tous : oblige REGEXP
+	} else {
+		// s'il y a plusieurs mots il faut les chercher tous : oblige REGEXP,
 		// sauf ceux de moins de 4 lettres (on supprime ainsi 'le', 'les', 'un',
 		// 'une', 'des' ...)
-		if (preg_match(",\s+,".$u, $recherche)){
+
+		// attention : plusieurs mots entre guillemets sont a rechercher tels quels
+		$recherche_trans = $recherche_mod = $recherche;
+
+		// les expressions entre " " sont un mot a chercher tel quel
+		// -> on remplace les espaces par un \x1 et on enleve les guillemets
+		if (preg_match(',["][^"]+["],Uims', $recherche_mod, $matches)) {
+			foreach ($matches as $match) {
+				$word = preg_replace(",\s+,Uims", "\x1", $match);
+				$word = trim($word, '"');
+				$recherche_mod = str_replace($match, $word, $recherche_mod);
+			}
+		}
+
+		if (preg_match(",\s+," . $u, $recherche_mod)) {
 			$is_preg = true;
+
 			$recherche_inter = '|';
-			$recherche_mots = explode(' ', $recherche);
+			$recherche_mots = explode(' ', $recherche_mod);
 			$min_long = defined('_RECHERCHE_MIN_CAR') ? _RECHERCHE_MIN_CAR : 4;
 			foreach ($recherche_mots as $mot) {
 				if (strlen($mot) >= $min_long) {
-					$recherche_inter .= $mot.' ';
+					// echapper les caracteres de regexp qui sont eventuellement dans la recherche
+					$recherche_inter .= preg_quote($mot) . ' ';
 				}
 			}
+			$recherche_inter = str_replace("\x1", '\s', $recherche_inter);
+
 			// mais on cherche quand même l'expression complète, même si elle
 			// comporte des mots de moins de quatre lettres
-			$recherche = rtrim($recherche.preg_replace(',\s+,'.$u, '|', $recherche_inter), '|');
+			$recherche = rtrim(preg_quote($recherche) . preg_replace(',\s+,' . $u, '|', $recherche_inter), '|');
 			$recherche_trans = translitteration($recherche);
 		}
 
-		$preg = '/'.str_replace('/', '\\/', $recherche_trans).'/' . $options['preg_flags'];
+		$preg = '/' . str_replace('/', '\\/', $recherche_trans) . '/' . $options['preg_flags'];
 	}
 
 	// Si la chaine est inactive, on va utiliser LIKE pour aller plus vite
 	// ou si l'expression reguliere est invalide
 	if (!$is_preg
-	  OR (@preg_match($preg,'')===FALSE) ) {
+		or (@preg_match($preg, '') === false)
+	) {
 		$methode = 'LIKE';
 		$u = $GLOBALS['meta']['pcre_u'];
+
+		// echapper les % et _
+		$q = str_replace(array('%', '_'), array('\%', '\_'), trim($recherche));
+
 		// eviter les parentheses et autres caractères qui interferent avec pcre par la suite (dans le preg_match_all) s'il y a des reponses
-		$recherche = str_replace(
-			array('(',')','?','[', ']', '+', '*', '/'),
-			array('\(','\)','[?]', '\[', '\]', '\+', '\*', '\/'),
-			$recherche);
+		$recherche = preg_quote($recherche, '/');
 		$recherche_trans = translitteration($recherche);
 		$recherche_mod = $recherche_trans;
 
-		// echapper les % et _
-		$q = str_replace(array('%','_'), array('\%', '\_'), trim($recherche));
 		// les expressions entre " " sont un mot a chercher tel quel
 		// -> on remplace les espaces par un _ et on enleve les guillemets
-		if (preg_match(',["][^"]+["],Uims',$q,$matches)){
-			foreach($matches as $match){
-				// corriger le like dans le $q
-				$word = preg_replace(",\s+,Uims","_",$match);
-				$word = trim($word,'"');
-				$q = str_replace($match,$word,$q);
-				// corriger la regexp
-				$word = preg_replace(",\s+,Uims","[\s]",$match);
-				$word = trim($word,'"');
-				$recherche_mod = str_replace($match,$word,$recherche_mod);
+		// corriger le like dans le $q
+		if (preg_match(',["][^"]+["],Uims', $q, $matches)) {
+			foreach ($matches as $match) {
+				$word = preg_replace(",\s+,Uims", "_", $match);
+				$word = trim($word, '"');
+				$q = str_replace($match, $word, $q);
+			}
+		}
+		// corriger la regexp
+		if (preg_match(',["][^"]+["],Uims', $recherche_mod, $matches)) {
+			foreach ($matches as $match) {
+				$word = preg_replace(",\s+,Uims", "[\s]", $match);
+				$word = trim($word, '"');
+				$recherche_mod = str_replace($match, $word, $recherche_mod);
 			}
 		}
 		$q = sql_quote(
 			"%"
-			. preg_replace(",\s+,".$u, "%", $q)
+			. preg_replace(",\s+," . $u, "%", $q)
 			. "%"
 		);
 
-		$preg = '/'.preg_replace(",\s+,".$u, ".+", trim($recherche_mod)).'/' . $options['preg_flags'];
+		$preg = '/' . preg_replace(",\s+," . $u, ".+", trim($recherche_mod)) . '/' . $options['preg_flags'];
 
 	} else {
 		$methode = 'REGEXP';
@@ -147,12 +170,13 @@ function expression_recherche($recherche, $options) {
 	// tous les caracteres transliterables de $q sont remplaces par un joker
 	// permet de matcher en SQL meme si on est sensible aux accents (SQLite)
 	$q_t = $q;
-	for($i = 0;$i<spip_strlen($q);$i++){
-		$char = spip_substr($q,$i,1);
+	for ($i = 0; $i < spip_strlen($q); $i++) {
+		$char = spip_substr($q, $i, 1);
 		if (!is_ascii($char)
-		  AND $char_t = translitteration($char)
-		  AND $char_t !== $char){
-			$q_t = str_replace($char,$is_preg?".":"_", $q_t);
+			and $char_t = translitteration($char)
+			and $char_t !== $char
+		) {
+			$q_t = str_replace($char, $is_preg ? "." : "_", $q_t);
 		}
 	}
 
@@ -161,12 +185,14 @@ function expression_recherche($recherche, $options) {
 	// fix : SQLite 3 est sensible aux accents, on jokerise les caracteres
 	// les plus frequents qui peuvent etre accentues
 	// (oui c'est tres dicustable...)
-	if (isset($GLOBALS['connexions'][$options['serveur']?$options['serveur']:0]['type'])
-	  AND strncmp($GLOBALS['connexions'][$options['serveur']?$options['serveur']:0]['type'],'sqlite',6)==0){
-		$q_t = strtr($q,"aeuioc",$is_preg?"......":"______");
+	if (isset($GLOBALS['connexions'][$options['serveur'] ? $options['serveur'] : 0]['type'])
+		and strncmp($GLOBALS['connexions'][$options['serveur'] ? $options['serveur'] : 0]['type'], 'sqlite', 6) == 0
+	) {
+		$q_t = strtr($q, "aeuioc", $is_preg ? "......" : "______");
 		// si il reste au moins un char significatif...
-		if (preg_match(",[^'%_.],",$q_t))
+		if (preg_match(",[^'%_.],", $q_t)) {
 			$q = $q_t;
+		}
 	}
 
 	return $expression[$key] = array($methode, $q, $preg);
