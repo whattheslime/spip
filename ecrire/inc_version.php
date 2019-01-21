3e1caf065268f0b4fa612f440d497ed93648fc6a
ben.spip@gmail.com <>
2019-01-21 14:10:02 +0000

    SPIP 3.2.3 tag spip

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -372,7 +372,7 @@ $liste_des_authentifications = array(
 // pour specifier les versions de SPIP necessaires
 // il faut s'en tenir a un nombre de decimales fixe
 // ex : 2.0.0, 2.0.0-dev, 2.0.0-beta, 2.0.0-beta2
-$spip_version_branche = "3.2.1";
+$spip_version_branche = "3.2.3";
 // cette version dev accepte tous les plugins compatible avec la version ci-dessous
 // a supprimer en phase beta/rc/release
 #define('_DEV_VERSION_SPIP_COMPAT',"3.1.3");
