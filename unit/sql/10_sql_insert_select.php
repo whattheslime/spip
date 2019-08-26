259aef1d5bb5cc78867a8d5e6532455e80efcb76
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-08-26 21:21:49 +0000

    Correction d’affichage d’un test sql_instert_select en cas d’erreur, et correction d’une coquille dans celui-ci.
    git-svn-id: https://zone.spip.org/spip-zone/_core_/tests@117643 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- unit/sql/10_sql_insert_select.php
+++ unit/sql/10_sql_insert_select.php
@@ -143,19 +143,21 @@ sleep(1); // sinon le timestamp ne change pas !
 		foreach (array('fr'=>'Aérien','en'=>'Aérieny') as $lg=>$res) {
 			$multi = sql_getfetsel(sql_multi("alcool",$lg),"spip_test_haddock","id_haddock=".sql_quote(2));
 			if (!($multi == ($res))) {
-				$err[] = "sql_multi $lg mal rendu : retour : ".htmlentities(utf8_decode($multi)).", attendu : ".htmlentities(utf8_decode($res));
+				$err[] = "sql_multi $lg mal rendu : retour : ".htmlentities($multi).", attendu : ".htmlentities($res);
 			}
 		}
 		// le bon texte avec multi et debut et fin de chaine
-		foreach (array('fr'=>'Un debut de chaine : Vinasse, et [la fin]','en'=>'Un début de chaine : Vinassy, et [la fin]','de'=>'Un début de chaine : Vinasse, et [la fin]',) as $lg=>$res) {
-			$multi = sql_getfetsel(sql_multi("alcool",$lg),"spip_test_haddock","id_haddock=".sql_quote(4));
-			if (!($multi == ($res))) {
-				$err[] = "sql_multi [$lg] mal rendu : retour : ".htmlentities(utf8_decode($multi)).", attendu : ".htmlentities(utf8_decode($res));
+		foreach (array(
+			'fr' => 'Un début de chaine : Vinasse, et [la fin]',
+			'en' => 'Un début de chaine : Vinassy, et [la fin]',
+			'de' => 'Un début de chaine : Vinasse, et [la fin]',
+		) as $lg => $res) {
+            $multi = sql_getfetsel(sql_multi("alcool",$lg),"spip_test_haddock","id_haddock=".sql_quote(4));
+            if (!($multi == ($res))) {
+                $err[] = "sql_multi [$lg] mal rendu : retour : ".htmlentities($multi).", attendu : ".htmlentities($res);
 			}
 		}
 
-
-								
 		// affichage
 		if ($err) {
 			return '<b>Selections</b><dl><dd>' . join('</dd><dd>', $err) . '</dd></dl>';
