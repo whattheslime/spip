ef4cdcbce075e8b2f1442647bdc30060897dc387
Matthieu Marcillaud <marcimat@rezo.net>
2019-08-27 20:05:58 +0200

    Ticket #4353 : On adapte les champs déclarés 'TIMESTAMP' en mysql versions récentes (8 par exemple) afin qu’ils se comportent comme on les attends dans SPIP, c’est à dire avec une mise à jour de la date à chaque update. On migre les champs des tables connues.
    Cependant, sur les tables crées auparavant avec une version de mysql récente, la colonne avec TIMESTAMP accepte les valeurs NULL et les accepte et en contient toujours après cette migration.
    Il faudrait peut être une autre migration pour appliquer une valeur sur tous les NULL pour pouvoir enlever cette indication dans la déclarationd du champ que nous n’avions pas auparavant.
    Autrement dit, on obtient 'TIMESTAMP NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP' à la place de 'TIMESTAMP default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'.
    Mais ça ne doit pas être très gênant.

--- ecrire/maj/svn10000.php
+++ ecrire/maj/svn10000.php
@@ -670,7 +670,7 @@ $GLOBALS['maj'][21676] = array(
 
 /**
  * Ranger les images de local/cache-gd2 dans des sous-rep
- * 
+ *
  * http://core.spip.net/issues/3277
  */
 function ranger_cache_gd2() {
@@ -715,3 +715,59 @@ $tables_objets_sql = lister_tables_objets_sql();
 foreach(array_keys($tables_objets_sql) as $table) {
 	$GLOBALS['maj'][24351][] = ['upgrade_logo_objet', objet_type($table)];
 };
+
+// adaptation des timestamp mysql
+$GLOBALS['maj'][24379] = [['maj_timestamp_mysql']];
+
+/**
+ * Mise à jour des bdd Mysql pour réparer les timestamp auto-update absents
+ *
+ * @uses base_lister_toutes_tables()
+ * @uses _mysql_remplacements_definitions_table()
+ **/
+function maj_timestamp_mysql($tables = null) {
+
+	include_spip('base/dump');
+	if (is_null($tables)) {
+		$tables = base_lister_toutes_tables();
+	} elseif (is_string($tables)) {
+		$tables = [$tables];
+	} elseif (!is_array($tables)) {
+		return;
+	}
+
+	// rien a faire si base non mysql
+	if (strncmp($GLOBALS['connexions'][0]['type'], 'mysql', 5) !== 0) {
+		return;
+	}
+
+	$trouver_table = charger_fonction('trouver_table', 'base');
+	// forcer le vidage de cache
+	$trouver_table('');
+
+	foreach ($tables as $table) {
+		if (time() >= _TIME_OUT) {
+			return;
+		}
+		if ($desc = $trouver_table($table)) {
+			$fields_corrected = _mysql_remplacements_definitions_table($desc['field']);
+			$d = array_diff($desc['field'], $fields_corrected);
+			if ($d) {
+				spip_log("Table $table TIMESTAMP incorrect", "maj");
+				foreach ($desc['field'] as $field => $type) {
+					if ($desc['field'][$field] !== $fields_corrected[$field]) {
+						spip_log("Adaptation TIMESTAMP table $table", "maj." . _LOG_INFO_IMPORTANTE);
+						sql_alter("table $table change $field $field " . $fields_corrected[$field]);
+						$trouver_table('');
+						$new_desc = $trouver_table($table);
+						spip_log("Apres conversion $table : " . var_export($new_desc['field'], true),
+							"maj." . _LOG_INFO_IMPORTANTE);
+					}
+				}
+			}
+		}
+	}
+
+	// forcer le vidage de cache
+	$trouver_table('');
+}
