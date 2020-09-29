40b1b5ed7200513fc9bcc4f6fdd70647a2d71484
Matthieu Marcillaud <marcimat@rezo.net>
2020-09-29 11:42:47 +0200

    Version 3.1.13

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -373,7 +373,12 @@ $liste_des_authentifications = array(
 // pour specifier les versions de SPIP necessaires
 // il faut s'en tenir a un nombre de decimales fixe
 // ex : 2.0.0, 2.0.0-dev, 2.0.0-beta, 2.0.0-beta2
-$spip_version_branche = "3.1.12";
+// le _SPIP_VERSION_ID est un nombre entier représentant le numéro de version (2 chiffres pour chaque 03 + 01 + 13 = 30113
+// le _SPIP_EXTRA_VERSION sert à repérer les version dev, beta etc. Pour une version stable il est vide.
+$spip_version_branche = "3.1.13";
+define('_SPIP_VERSION_ID', 30113);
+define('_SPIP_EXTRA_VERSION', '');
+
 // version des signatures de fonctions PHP
 // (= numero SVN de leur derniere modif cassant la compatibilite et/ou necessitant un recalcul des squelettes)
 $spip_version_code = 22653;
