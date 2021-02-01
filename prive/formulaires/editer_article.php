0653d605abe44ea4cb3f7ba5783e26cb29a36342
Cerdic <cedric@yterium.com>
2021-02-01 15:46:43 +0100

    appels a autoriser sur les formulaires editer, a minima quand on les utilise en modification d'un objet existant

--- prive/formulaires/editer_article.php
+++ prive/formulaires/editer_article.php
@@ -64,6 +64,11 @@ function formulaires_editer_article_charger_dist(
 		$row,
 		$hidden
 	);
+
+	if (intval($id_article) and !autoriser('modifier', 'article', intval($id_article))) {
+		$valeurs['editable'] = '';
+	}
+
 	// il faut enlever l'id_rubrique car la saisie se fait sur id_parent
 	// et id_rubrique peut etre passe dans l'url comme rubrique parent initiale
 	// et sera perdue si elle est supposee saisie
