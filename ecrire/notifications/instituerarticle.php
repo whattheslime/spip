875ae4f6c42f95f008eb8422a28b2770c90f0512
Cerdic <cedric@yterium.com>
2021-02-12 17:25:52 +0100

    Fix #4524 un cartouche manquant

--- ecrire/notifications/instituerarticle.php
+++ ecrire/notifications/instituerarticle.php
@@ -1,10 +1,14 @@
 <?php
-/*
- * Plugin Notifications
- * (c) 2009 SPIP
- * Distribue sous licence GPL
- *
- */
+
+/***************************************************************************\
+ *  SPIP, Système de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright © avec tendresse depuis 2001                                 *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Rivière, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribué sous licence GNU/GPL.     *
+ *  Pour plus de détails voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
 
 
 if (!defined('_ECRIRE_INC_VERSION')) {
