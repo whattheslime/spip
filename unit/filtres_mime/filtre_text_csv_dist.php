9cd715d20a3e1d703c2bb16491bf38a5a1f8106c
Cerdic <cedric@yterium.com>
2021-05-07 12:09:29 +0200

    L'analyse du CSV produit maintenant un vrai guillemet et plus un &#34; depuis le bugfix https://git.spip.net/spip/spip/commit/add53f32cde376bd7224553c068f810beca90c4a

--- unit/filtres_mime/filtre_text_csv_dist.php
+++ unit/filtres_mime/filtre_text_csv_dist.php
@@ -33,29 +33,29 @@
   0 => 
   array (
     0 => '<table class="table spip">
-<thead><tr class=\'row_first\'><th id=\'id8549_c0\'>A</th><th id=\'id8549_c1\'>B</th><th id=\'id8549_c2\'>C</th><th id=\'id8549_c3\'>D</th><th id=\'id8549_c4\'>E</th><th id=\'id8549_c5\'>F</th></tr></thead>
+<thead><tr class=\'row_first\'><th id=\'id9b86_c0\'>A</th><th id=\'id9b86_c1\'>B</th><th id=\'id9b86_c2\'>C</th><th id=\'id9b86_c3\'>D</th><th id=\'id9b86_c4\'>E</th><th id=\'id9b86_c5\'>F</th></tr></thead>
 <tbody>
 <tr class=\'row_odd odd\'>
-<td headers=\'id8549_c0\'>un</td>
-<td headers=\'id8549_c1\'>tableau</td>
-<td headers=\'id8549_c2\'>csv</td>
-<td headers=\'id8549_c3\'>avec</td>
-<td headers=\'id8549_c4\'>des</td>
-<td headers=\'id8549_c5\'>valeurs</td></tr>
+<td headers=\'id9b86_c0\'>un</td>
+<td headers=\'id9b86_c1\'>tableau</td>
+<td headers=\'id9b86_c2\'>csv</td>
+<td headers=\'id9b86_c3\'>avec</td>
+<td headers=\'id9b86_c4\'>des</td>
+<td headers=\'id9b86_c5\'>valeurs</td></tr>
 <tr class=\'row_even even\'>
-<td headers=\'id8549_c0\'>dans chaque</td>
-<td headers=\'id8549_c1\'>case</td>
-<td headers=\'id8549_c2\'>et aussi une</td>
-<td headers=\'id8549_c3\'>case</td>
-<td headers=\'id8549_c4\'>avec</td>
-<td headers=\'id8549_c5\'>des</td></tr>
+<td headers=\'id9b86_c0\'>dans chaque</td>
+<td headers=\'id9b86_c1\'>case</td>
+<td headers=\'id9b86_c2\'>et aussi une</td>
+<td headers=\'id9b86_c3\'>case</td>
+<td headers=\'id9b86_c4\'>avec</td>
+<td headers=\'id9b86_c5\'>des</td></tr>
 <tr class=\'row_odd odd\'>
-<td headers=\'id8549_c0\'>&#34;guillemets&#34;</td>
-<td headers=\'id8549_c1\'>est-ce</td>
-<td headers=\'id8549_c2\'>que</td>
-<td headers=\'id8549_c3\'>ça</td>
-<td headers=\'id8549_c4\'>marche&nbsp;?</td>
-<td headers=\'id8549_c5\'></td></tr>
+<td headers=\'id9b86_c0\'>"guillemets"</td>
+<td headers=\'id9b86_c1\'>est-ce</td>
+<td headers=\'id9b86_c2\'>que</td>
+<td headers=\'id9b86_c3\'>ça</td>
+<td headers=\'id9b86_c4\'>marche&nbsp;?</td>
+<td headers=\'id9b86_c5\'></td></tr>
 </tbody>
 </table>',
     1 => 'A;B;C;D;E;F
@@ -99,29 +99,29 @@ guillemets;est-ce;que;ça;marche ?;',
   2 => 
   array (
     0 => '<table class="table spip">
-<thead><tr class=\'row_first\'><th id=\'idcc66_c0\'>A</th><th id=\'idcc66_c1\'>B</th><th id=\'idcc66_c2\'>C</th><th id=\'idcc66_c3\'>D</th><th id=\'idcc66_c4\'>E</th><th id=\'idcc66_c5\'>F</th></tr></thead>
+<thead><tr class=\'row_first\'><th id=\'idee6c_c0\'>A</th><th id=\'idee6c_c1\'>B</th><th id=\'idee6c_c2\'>C</th><th id=\'idee6c_c3\'>D</th><th id=\'idee6c_c4\'>E</th><th id=\'idee6c_c5\'>F</th></tr></thead>
 <tbody>
 <tr class=\'row_odd odd\'>
-<td headers=\'idcc66_c0\'>un</td>
-<td headers=\'idcc66_c1\'>tableau</td>
-<td headers=\'idcc66_c2\'>csv</td>
-<td headers=\'idcc66_c3\'>avec</td>
-<td headers=\'idcc66_c4\'>des</td>
-<td headers=\'idcc66_c5\'>valeurs</td></tr>
+<td headers=\'idee6c_c0\'>un</td>
+<td headers=\'idee6c_c1\'>tableau</td>
+<td headers=\'idee6c_c2\'>csv</td>
+<td headers=\'idee6c_c3\'>avec</td>
+<td headers=\'idee6c_c4\'>des</td>
+<td headers=\'idee6c_c5\'>valeurs</td></tr>
 <tr class=\'row_even even\'>
-<td headers=\'idcc66_c0\'>dans chaque</td>
-<td headers=\'idcc66_c1\'>case</td>
-<td headers=\'idcc66_c2\'>et aussi une</td>
-<td headers=\'idcc66_c3\'>case</td>
-<td headers=\'idcc66_c4\'>avec</td>
-<td headers=\'idcc66_c5\'>des</td></tr>
+<td headers=\'idee6c_c0\'>dans chaque</td>
+<td headers=\'idee6c_c1\'>case</td>
+<td headers=\'idee6c_c2\'>et aussi une</td>
+<td headers=\'idee6c_c3\'>case</td>
+<td headers=\'idee6c_c4\'>avec</td>
+<td headers=\'idee6c_c5\'>des</td></tr>
 <tr class=\'row_odd odd\'>
-<td headers=\'idcc66_c0\'>&#34;guillemets&#34;</td>
-<td headers=\'idcc66_c1\'>est-ce</td>
-<td headers=\'idcc66_c2\'>que</td>
-<td headers=\'idcc66_c3\'>√ßa</td>
-<td headers=\'idcc66_c4\'>marche&nbsp;?</td>
-<td headers=\'idcc66_c5\'></td></tr>
+<td headers=\'idee6c_c0\'>"guillemets"</td>
+<td headers=\'idee6c_c1\'>est-ce</td>
+<td headers=\'idee6c_c2\'>que</td>
+<td headers=\'idee6c_c3\'>√ßa</td>
+<td headers=\'idee6c_c4\'>marche&nbsp;?</td>
+<td headers=\'idee6c_c5\'></td></tr>
 </tbody>
 </table>',
     1 => '"A","B","C","D","E","F"
