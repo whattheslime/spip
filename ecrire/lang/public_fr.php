d9d7707277452e39c8fc7a9165be20ce46f21e2a
Matthieu Marcillaud <marcimat@rezo.net>
2021-06-01 11:07:14 +0200

    Ticket !spip/dist/pulls/1 : Chaines de langues relatifs à l'ajout d'une feuille xslt pour le flux rss des squelettes-dist.

--- ecrire/lang/public_fr.php
+++ ecrire/lang/public_fr.php
@@ -94,6 +94,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'repondre_breve' => 'Répondre à cette brève',
 	'resultats_recherche' => 'Résultats de la recherche',
 	'retour_debut_forums' => 'Retour au début des forums',
+	'rss_abonnement' => 'Copiez l’URL suivante dans votre agrégateur :',
+	'rss_abonnement_titre' => 'S’abonner',
+	'rss_abonnement_titre_page' => 'S’abonner à',
+	'rss_explication' => "Un fil RSS recueille les informations de mise à jour d'un site. Il fournit le contenu des billets ou des commentaires ou un extrait de ceux-ci, ainsi qu'un lien vers les versions complètes et quelques autres informations. Ce fil a pour vocation d'être lu par un agrégateur RSS.",
+	'rss_explication_titre' => 'Qu’est-ce qu’un flux RSS ?',
 	'rubrique' => 'Rubrique',
 	'rubriques' => 'Rubriques',
 
