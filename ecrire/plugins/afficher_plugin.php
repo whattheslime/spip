0127052db86d9bd369713cc1363eb5614c8e6e5d
Cerdic <cedric@yterium.com>
2021-07-07 15:21:06 +0200

    Fix les logos -xx.svg des plugins quand le png n'est pas fourni

--- ecrire/plugins/afficher_plugin.php
+++ ecrire/plugins/afficher_plugin.php
@@ -167,17 +167,16 @@ function plugin_resume($info, $dir_plugins, $plug_file, $url_page) {
 	$icon_class = 'icon';
 	$img = '';
 	if (isset($info['logo']) and $i = trim($info['logo'])) {
-		if ($img = chemin_image("$dir/$i")) {
-			$img = http_img_pack($img, '', " width='32' height='32'", '', ['variante_svg_si_possible' => true, 'chemin_image' => false]);
+		$img = http_img_pack("$dir/$i", '', " width='32' height='32'", '', ['variante_svg_si_possible' => true, 'chemin_image' => false]);
+		if (!extraire_attribut($img, 'src')) {
+			$img = '';
 		}
 	}
 	if (!$img) {
 		$img = http_img_pack("plugin-xx.svg", '', " width='32' height='32'");
 		$icon_class .= ' no-logo';
 	}
-	else {
-#		$img = "<img src='$img' width='32' height='32' alt='' />";
-	}
+
 	$i = "<div class='$icon_class'><a href='$url' rel='info'>$img</a></div>";
 
 	return "<div class='resume'>"
