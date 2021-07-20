ea81983458600244b1b66ddb1dc63a4d9046bd1c
Cerdic <cedric@yterium.com>
2021-07-20 16:22:56 +0200

    Le cartouche et le namespace de SPIP...

--- tests/LegacyUnitPhpTest.php
+++ tests/LegacyUnitPhpTest.php
@@ -1,16 +1,16 @@
 <?php
 
-/**
- * SCSSPHP
- *
- * @copyright 2012-2020 Leaf Corcoran
- *
- * @license http://opensource.org/licenses/MIT MIT
- *
- * @link http://scssphp.github.io/scssphp
- */
+/***************************************************************************\
+ *  SPIP, Système de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright © avec tendresse depuis 2001                                 *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Rivière, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribué sous licence GNU/GPL.     *
+ *  Pour plus de détails voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
 
-namespace ScssPhp\ScssPhp\Tests;
+namespace Spip\Core\Tests;
 
 use PHPUnit\Framework\TestCase;
 
