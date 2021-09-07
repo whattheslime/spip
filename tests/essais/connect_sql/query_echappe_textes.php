0df11402625712ac4a224b0a5bce215b3be153eb
Cerdic <cedric@yterium.com>
2021-09-07 23:04:37 +0200

    Mise a jour du test sur query_echappe_textes()

--- tests/essais/connect_sql/query_echappe_textes.php
+++ tests/essais/connect_sql/query_echappe_textes.php
@@ -8,6 +8,10 @@ namespace Spip\Core\Tests;
 
 find_in_path("base/connect_sql.php",'',true);
 
+function pretest_connect_sql_query_echappe_textes() {
+	query_echappe_textes('', 'uniqid');
+}
+
 /**
  * La fonction appelee pour chaque jeu de test
  * Nommage conventionnel : test_[[dossier1_][[dossier2_]...]]fichier
@@ -26,6 +30,7 @@ function test_connect_sql_query_echappe_textes(...$args) {
  *  [ output, input1, input2, input3...]
  */
 function essais_connect_sql_query_echappe_textes(){
+	  $md5 = substr(md5('uniqid'), 0, 4);
 		$essais = array (
   array (
     0 => array('%1$s', array ("'guillemets simples'")),
@@ -44,19 +49,19 @@ function essais_connect_sql_query_echappe_textes(){
     1 => "\"guillemets doubles 1/2\",\"guillemets doubles 2/2\"",
   ),
   array (
-    0 => array('%1$s', array ("'guillemets simples \x2@##@\x2 avec un echappement'")),
+    0 => array('%1$s', array ("'guillemets simples \x2@#{$md5}#@\x2 avec un echappement'")),
     1 => "'guillemets simples \' avec un echappement'",
   ),
   array (
-    0 => array('%1$s', array ("\"guillemets doubles \x3@##@\x3 avec un echappement\"")),
+    0 => array('%1$s', array ("\"guillemets doubles \x3@#{$md5}#@\x3 avec un echappement\"")),
     1 => "\"guillemets doubles \\\" avec un echappement\"",
   ),
   array (
-    0 => array('%1$s', array ("'guillemets simples \x2@##@\x2\x3@##@\x3 avec deux echappements'")),
+    0 => array('%1$s', array ("'guillemets simples \x2@#{$md5}#@\x2\x3@#{$md5}#@\x3 avec deux echappements'")),
     1 => "'guillemets simples \'\\\" avec deux echappements'",
   ),
   array (
-    0 => array('%1$s', array ("\"guillemets doubles \x2@##@\x2\x3@##@\x3 avec deux echappements\"")),
+    0 => array('%1$s', array ("\"guillemets doubles \x2@#{$md5}#@\x2\x3@#{$md5}#@\x3 avec deux echappements\"")),
     1 => "\"guillemets doubles \'\\\" avec deux echappements\"",
   ),
   array (
