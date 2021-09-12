d3e5450f8bde85be748ec59b171fc8bf5dc748e7
Matthieu Marcillaud <marcimat@rezo.net>
2021-09-13 00:58:53 +0200

    Entêtes en utf8

--- tests/ActionEditerLiensTest.php
+++ tests/ActionEditerLiensTest.php
@@ -1,14 +1,14 @@
 <?php
 
 /***************************************************************************\
- *  SPIP, Système de publication pour l'internet                           *
+ *  SPIP, SystÃ¨me de publication pour l'internet                           *
  *                                                                         *
- *  Copyright © avec tendresse depuis 2001                                 *
- *  Arnaud Martin, Antoine Pitrou, Philippe Rivière, Emmanuel Saint-James  *
+ *  Copyright Â© avec tendresse depuis 2001                                 *
+ *  Arnaud Martin, Antoine Pitrou, Philippe RiviÃ¨re, Emmanuel Saint-James  *
  *                                                                         *
- *  Ce programme est un logiciel libre distribué sous licence GNU/GPL.     *
- *  Pour plus de détails voir le fichier COPYING.txt ou l'aide en ligne.   *
- * \***************************************************************************/
+ *  Ce programme est un logiciel libre distribuÃ© sous licence GNU/GPL.     *
+ *  Pour plus de dÃ©tails voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
 
 namespace Spip\Core\Tests;
 
