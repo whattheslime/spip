aefb90d6a186f81c2596dc39a010a5827921b6c1
Cerdic <cedric@yterium.com>
2021-10-21 11:14:18 +0200

    Il faut incrementer spip_version_code car tous les formulaires doivent etre recalcules

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -382,8 +382,8 @@ define('_SPIP_EXTRA_VERSION', '');
 // a supprimer en phase beta/rc/release
 #define('_DEV_VERSION_SPIP_COMPAT',"3.1.3");
 // version des signatures de fonctions PHP
-// (= numero SVN de leur derniere modif cassant la compatibilite et/ou necessitant un recalcul des squelettes)
-$spip_version_code = 22653;
+// (= date de leur derniere modif cassant la compatibilite et/ou necessitant un recalcul des squelettes)
+$spip_version_code = 20211021;
 // version de la base SQL (= numero SVN de sa derniere modif)
 $spip_version_base = 24379;
 
