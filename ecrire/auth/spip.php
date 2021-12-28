f6d1d31a8d9d7a5fa678a2afb85d4a51d1f3ea7e
b_b <bruno@eliaz.fr>
2021-12-28 11:34:33 +0100

    revert !486 & d8d83b4ce5 puisque generer_htpass() delegue à la fonction inc_generer_htpass_dist() si elle existe
    thx @cedric

--- ecrire/auth/spip.php
+++ ecrire/auth/spip.php
@@ -411,11 +411,7 @@ function auth_spip_modifier_pass($login, $new_pass, $id_auteur, $serveur = '') {
 	$c = [];
 	include_spip('inc/acces');
 	include_spip('auth/sha256.inc');
-	if ($generer_htpass = charger_fonction('generer_htpass', 'inc', true)) {
-		$htpass = $generer_htpass($new_pass);
-	} else {
-		$htpass = '';
-	}
+	$htpass = generer_htpass($new_pass);
 	$alea_actuel = creer_uniqid();
 	$alea_futur = creer_uniqid();
 	$pass = spip_sha256($alea_actuel . $new_pass);
