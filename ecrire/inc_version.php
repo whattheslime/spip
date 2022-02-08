7bffd54916d39754c74315516d9ed0d5acbe6fd3
Matthieu Marcillaud <marcimat@rezo.net>
2022-02-08 22:04:06 +0100

    Version 4.1.0-alpha

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -440,13 +440,13 @@ $liste_des_authentifications = [
 // pour specifier les versions de SPIP necessaires
 // il faut s'en tenir a un nombre de decimales fixe
 // ex : 2.0.0, 2.0.0-dev, 2.0.0-beta, 2.0.0-beta2
-$spip_version_branche = '4.1.0-dev';
+$spip_version_branche = '4.1.0-alpha';
 define('_SPIP_VERSION_ID', 41000);
-define('_SPIP_EXTRA_VERSION', '-dev');
+define('_SPIP_EXTRA_VERSION', '-alpha');
 
 // cette version dev accepte tous les plugins compatible avec la version ci-dessous
 // a supprimer en phase beta/rc/release
-define('_DEV_VERSION_SPIP_COMPAT', '4.0.99');
+#define('_DEV_VERSION_SPIP_COMPAT', '4.0.99');
 // version des signatures de fonctions PHP
 // (= date de leur derniere modif cassant la compatibilite et/ou necessitant un recalcul des squelettes)
 $spip_version_code = 2022_01_16;
