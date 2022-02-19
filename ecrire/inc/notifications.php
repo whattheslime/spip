fd89189c0e783d9c77d029ea6ccca47ff2b90e2b
Eric Lupinacci <eric@smellup.net>
2022-02-19 14:43:29 +0100

    Eviter un warning en ajoutant un include manquant
    (cherry picked from commit 3ce5b0fe04542226ef04cb4040ea3f2c5859f9c5)

--- ecrire/inc/notifications.php
+++ ecrire/inc/notifications.php
@@ -57,6 +57,7 @@ function inc_notifications_dist($quoi, $id = 0, $options = []) {
  */
 function notifications_nettoyer_emails(&$emails, $exclure = []) {
 	// filtrer et unifier
+	include_spip('inc/filtres');
 	$emails = array_unique(array_filter(array_map('email_valide', array_map('trim', $emails))));
 	if ($exclure and count($exclure)) {
 		// nettoyer les exclusions d'abord
