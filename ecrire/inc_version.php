51d9483bb643d37ff99eb0126be7d2b45de6aee5
Matthieu Marcillaud <marcimat@rezo.net>
2022-04-01 10:44:49 +0200

    Coquille dans _SPIP_VERSION_ID Nous sommes en version 4.2 ici, pas 42…

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -442,7 +442,7 @@ $liste_des_authentifications = [
 // il faut s'en tenir a un nombre de decimales fixe
 // ex : 2.0.0, 2.0.0-dev, 2.0.0-beta, 2.0.0-beta2
 $spip_version_branche = '4.2.0-dev';
-define('_SPIP_VERSION_ID', 42000);
+define('_SPIP_VERSION_ID', 40200);
 define('_SPIP_EXTRA_VERSION', '-dev');
 
 // cette version dev accepte tous les plugins compatible avec la version ci-dessous
