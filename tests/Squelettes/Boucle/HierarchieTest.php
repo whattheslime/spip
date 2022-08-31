1f25d670e99c92144f36cceca15207c26ab5db60
Matthieu Marcillaud <marcimat@rezo.net>
2022-08-31 16:02:10 +0200

    refactor: Ces doublons là sont des critères

--- tests/Squelettes/Boucle/HierarchieTest.php
+++ tests/Squelettes/Boucle/HierarchieTest.php
@@ -8,7 +8,31 @@ use Spip\Core\Testing\Template\StringLoader;
 use Spip\Core\Testing\Template\FileLoader;
 
 class HierarchieTest extends SquelettesTestCase {
-	public function testHierarchiesMultiples(): void {
-		$this->assertOkSquelette(__DIR__ . '/data/hierarchies_multiples.html');
+	function testBoucleHierarchie(){
+		$this->assertOkCode("
+			<BOUCLE_a(RUBRIQUES){0,50}>
+			<BOUCLE_secteur(HIERARCHIE){0,1}>[(#ID_RUBRIQUE|=={#_a:ID_SECTEUR}|?{'',
+				erreur secteur: attendu #_a:ID_SECTEUR ; resultat #ID_RUBRIQUE<br />})]
+			</BOUCLE_secteur>
+			<BOUCLE_parent(HIERARCHIE){n-1,1}>[(#ID_RUBRIQUE|=={#_a:ID_PARENT}|?{'',
+				erreur parent: attendu #_a:ID_PARENT ; resultat #ID_RUBRIQUE<br />})]
+			</BOUCLE_parent>
+			</BOUCLE_a>
+			OK
+		");
+	}
+
+	function testBoucleHierarchieTout(){
+		$this->assertOkCode("
+			<BOUCLE_a(RUBRIQUES){tout}{0,50}>
+			<BOUCLE_secteur(HIERARCHIE){statut==.*}{0,1}>[(#ID_RUBRIQUE|=={#_a:ID_SECTEUR}|?{'',
+				erreur secteur: attendu #_a:ID_SECTEUR ; resultat #ID_RUBRIQUE<br />})]
+			</BOUCLE_secteur>
+			<BOUCLE_parent(HIERARCHIE){statut==.*}{n-1,1}>[(#ID_RUBRIQUE|=={#_a:ID_PARENT}|?{'',
+				erreur parent: attendu #_a:ID_PARENT ; resultat #ID_RUBRIQUE<br />})]
+			</BOUCLE_parent>
+			</BOUCLE_a>
+			OK
+		");
 	}
 }
