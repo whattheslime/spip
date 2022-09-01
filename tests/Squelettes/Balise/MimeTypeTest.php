c2623c54507cb37fb0e046587218aaad43c20564
Matthieu Marcillaud <marcimat@rezo.net>
2022-09-01 16:50:40 +0000

    refactor: Tests de balise MimeType (à déplacer dans Medias) en PHPUnit

---
+++ tests/Squelettes/Balise/MimeTypeTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Spip\Core\Tests\Squelettes\Balise;
+
+use Spip\Core\Testing\SquelettesTestCase;
+use Spip\Core\Testing\Templating;
+
+/** FIXME: Déplacer dans le plugin Medias  */
+class MimeTypeTest extends SquelettesTestCase
+{
+	function testMimeTypeDocumentJpg(){
+		$templating = Templating::fromString();
+		$result = $templating->render(
+			"<BOUCLE_d(DOCUMENTS){extension IN jpg}{0,1}>
+				[(#MIME_TYPE|match{^image/jpeg$}|?{OK, erreur mime_type : #MIME_TYPE})]
+			</BOUCLE_d>
+			NA Ce test ne fonctionne que s'il y a au moins un document jpg dans le site !
+			<//B_d>"
+		);
+		if ($this->isNa($result)) {
+			$this->markTestSkipped($result);
+		}
+		$this->assertOkCode($result);
+	}
+}
+
