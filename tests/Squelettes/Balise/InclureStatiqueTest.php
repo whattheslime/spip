3fcaa1941ef69556a440a29cb62de8f80dac9031
Matthieu Marcillaud <marcimat@rezo.net>
2022-09-01 14:05:56 +0000

    refactor: Tests de balise Inclure en PHPUnit

---
+++ tests/Squelettes/Balise/InclureStatiqueTest.php
@@ -0,0 +1,72 @@
+<?php
+namespace Spip\Core\Tests\Squelettes\Balise;
+
+use Spip\Core\Testing\SquelettesTestCase;
+use Spip\Core\Testing\Templating;
+use Spip\Core\Testing\Template\StringLoader;
+use Spip\Core\Testing\Template\FileLoader;
+
+class InclureStatiqueTest extends SquelettesTestCase
+{
+	public function testInclureInlineNormal(): void {
+		$dir = substr(__DIR__, strlen(_SPIP_TEST_CHDIR) + 1);
+		$this->assertEqualsCode('Hello World', '#INCLURE{fond=' . $dir . '/data/texte_hello_world}');
+		$this->assertEqualsCode('Hello World', '[(#INCLURE{fond=' . $dir . '/data/texte_hello_world})]');
+	}
+
+	public function testInclureDouble(): void {
+		$dir = substr(__DIR__, strlen(_SPIP_TEST_CHDIR) + 1);
+		$this->assertEqualsCode(
+			'Hello WorldHello World',
+			'#INCLURE{fond=' . $dir . '/data/texte_hello_world}'
+			. '#INCLURE{fond=' . $dir . '/data/texte_hello_world}'
+		);
+		$this->assertEqualsCode(
+			'Hello WorldHello World',
+			'
+			#INCLURE{fond=' . $dir . '/data/texte_hello_world}'
+			. '#INCLURE{fond=' . $dir . '/data/texte_hello_world}'
+		);
+	}
+
+	public function testInclureArray(): void {
+		$dir = substr(__DIR__, strlen(_SPIP_TEST_CHDIR) + 1);
+		$array = '#LISTE{
+			' . $dir . '/data/texte_hello_world,
+			' . $dir . '/data/texte_hello_world,
+			' . $dir . '/data/texte_hello_world}';
+		$this->assertEqualsCode('Hello WorldHello WorldHello World', "#INCLURE{fond=$array}");
+	}	
+	
+	public function testInclureOldParam(): void {
+		$dir = substr(__DIR__, strlen(_SPIP_TEST_CHDIR) + 1);
+		$this->assertEqualsCode('Kitty', '[(#INCLURE{fond=' . $dir . '/data/balise_env_test}{test=Kitty})]');
+		$this->assertEqualsCode('Kitty', '[(#INCLURE{fond=' . $dir . '/data/balise_env_test}{test=Kitty})]');
+	}
+
+	public function testInclureNormalParam(): void {
+		$dir = substr(__DIR__, strlen(_SPIP_TEST_CHDIR) + 1);
+		$this->assertEqualsCode('Kitty','[(#INCLURE{fond=' . $dir . '/data/balise_env_test, test=Kitty})]');
+		$this->assertEqualsCode('Kitty','[(#INCLURE{fond=' . $dir . '/data/balise_env_test, test=Kitty})]');
+	}
+	
+	public function testInclureArrayParam(): void {
+		$dir = substr(__DIR__, strlen(_SPIP_TEST_CHDIR) + 1);
+		$array = '#LISTE{
+			' . $dir . '/data/balise_env_test,
+			' . $dir . '/data/texte_hello_world,
+			' . $dir . '/data/balise_env_test}';
+		$this->assertEqualsCode('KittyHello WorldKitty', "[(#INCLURE{fond=$array, test=Kitty})]");
+		$this->assertEqualsCode('KittyHello WorldKitty', "[(#INCLURE{fond=$array, test=Kitty})]");
+	}
+
+	/**
+	 * Un inclure manquant doit creer une erreur de compilation pour SPIP
+	 * qui ne doivent pas s'afficher dans le public si visiteur
+	 */ 
+	public function testInclureManquantGenereErreurCompilation(): void {
+		$templating = Templating::fromString();
+		$infos = $templating->rawRender('#CACHE{0}[(#INCLURE{fond=carabistouille/de/montignac/absente}|non)ok]');
+		$this->assertCount(1, $infos['erreurs']);
+	}
+}
--- tests/Testing/TemplateTest.php
+++ tests/Testing/TemplateTest.php
@@ -9,12 +9,12 @@ class TemplateTest extends SquelettesTestCase
 {
 	public function testNativeRecupererFond(): void {
 		$dir = substr(__DIR__, strlen(_SPIP_TEST_CHDIR) + 1);
-		$this->assertEquals('Hello World', recuperer_fond($dir . '/data/inclus_hello_world'));
+		$this->assertEquals('Hello World', recuperer_fond($dir . '/data/texte_hello_world'));
 	}
 
 	public function testRenderer(): void {
 		$dir = substr(__DIR__, strlen(_SPIP_TEST_CHDIR) + 1);
-		$template = new Template($dir . '/data/inclus_hello_world');
+		$template = new Template($dir . '/data/texte_hello_world');
 		$this->assertEquals('Hello World', $template->render());
 	}
 }
