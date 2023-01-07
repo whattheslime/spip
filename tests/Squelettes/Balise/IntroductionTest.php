1581222fb69099f7d9fe985efabaac6cfbc90d2b
Cerdic <cedric@yterium.com>
2023-01-07 17:07:58 +0100

    refactor: utiliser une requete SQL pour selectionner l'article de test, c'est plus simple
    Refs: https://git.spip.net/spip/tests/pulls/10

--- tests/Squelettes/Balise/IntroductionTest.php
+++ tests/Squelettes/Balise/IntroductionTest.php
@@ -9,11 +9,25 @@ use Spip\Core\Testing\Templating;
 
 class IntroductionTest extends SquelettesTestCase
 {
+	protected function getIdArticleLong() : int
+	{
+		include_spip('base/abstract_sql');
+		$id_article = sql_getfetsel('id_article',
+			'spip_articles',
+			"descriptif='' AND LENGTH(CONCAT(chapo, texte)) > 520 AND texte!='' AND LENGTH(chapo) > 100",
+			'',
+			'id_article',
+			'0,1'
+		);
+		return intval($id_article);
+	}
+
 	public function testArticleLongExiste(): void
 	{
 		$templating = Templating::fromString();
-		$code = "<BOUCLE_a(ARTICLES){chapo=='.{100}'}{texte!=''}{descriptif=''}{0,1}>OK</BOUCLE_a>NA<//B_a>";
-		$result = $templating->render($code);
+		$id_article = $this->getIdArticleLong();
+		$code = "<BOUCLE_a(ARTICLES){id_article}{tout}>OK</BOUCLE_a>NA<//B_a>";
+		$result = $templating->render($code, ['id_article' => $id_article]);
 		if ($this->isNA($result)) {
 			$this->markTestSkipped($result);
 		}
@@ -27,8 +41,9 @@ class IntroductionTest extends SquelettesTestCase
 	public function testCoupeIntroduction(): void
 	{
 		$templating = Templating::fromString();
-		$code = "<BOUCLE_a(ARTICLES){chapo=='.{100}'}{texte!=''}{descriptif=''}{0,1}>#INTRODUCTION</BOUCLE_a>";
-		$result = $templating->render($code);
+		$id_article = $this->getIdArticleLong();
+		$code = "<BOUCLE_a(ARTICLES){id_article}{tout}{0,1}>#INTRODUCTION</BOUCLE_a>";
+		$result = $templating->render($code, ['id_article' => $id_article]);
 		$suite = '&nbsp;(...)';
 		$this->assertMatchesRegularExpression('#' . preg_quote($suite . '</p>', '#') . '$#', $result);
 	}
@@ -39,13 +54,15 @@ class IntroductionTest extends SquelettesTestCase
 	public function testCoupeIntroductionSuite(): void
 	{
 		$templating = Templating::fromString();
-		$code = "<BOUCLE_a(ARTICLES){chapo=='.{100}'}{texte!=''}{descriptif=''}{0,1}>#INTRODUCTION{…}</BOUCLE_a>";
-		$result = $templating->render($code);
+		$id_article = $this->getIdArticleLong();
+		$code = "<BOUCLE_a(ARTICLES){id_article}{tout}{0,1}>#INTRODUCTION{…}</BOUCLE_a>";
+		$result = $templating->render($code, ['id_article' => $id_article]);
 		$suite = '…';
 		$this->assertMatchesRegularExpression('#' . preg_quote($suite . '</p>', '#') . '$#', $result);
 
-		$code = "<BOUCLE_a(ARTICLES){chapo=='.{100}'}{texte!=''}{descriptif=''}{0,1}>#INTRODUCTION{#ENV{suite}}</BOUCLE_a>";
+		$code = "<BOUCLE_a(ARTICLES){id_article}{tout}{0,1}>#INTRODUCTION{#ENV{suite}}</BOUCLE_a>";
 		$result = $templating->render($code, [
+			'id_article' => $id_article,
 			'suite' => $suite,
 		]);
 		$this->assertMatchesRegularExpression('#' . preg_quote($suite . '</p>', '#') . '$#', $result);
@@ -56,6 +73,7 @@ class IntroductionTest extends SquelettesTestCase
 	 */
 	public function testCoupeIntroductionConstante(): void
 	{
+		$id_article = $this->getIdArticleLong();
 		$templating = Templating::fromString([
 			'fonctions' => "
 				if (!defined('_INTRODUCTION_SUITE')) {
@@ -63,8 +81,8 @@ class IntroductionTest extends SquelettesTestCase
 				}
 			",
 		]);
-		$code = "#CACHE{0}<BOUCLE_a(ARTICLES){chapo=='.{100}'}{texte!=''}{descriptif=''}{0,1}>#INTRODUCTION</BOUCLE_a>";
-		$result = $templating->render($code);
+		$code = "#CACHE{0}<BOUCLE_a(ARTICLES){id_article}{tout}{0,1}>#INTRODUCTION</BOUCLE_a>";
+		$result = $templating->render($code, ['id_article' => $id_article]);
 		$suite = _INTRODUCTION_SUITE;
 		$this->assertMatchesRegularExpression('#' . preg_quote($suite . '</p>', '#') . '$#', $result);
 	}
