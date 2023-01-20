f21f4b0cf5a83b15372dd3348fb29c47ad864ccd
Matthieu Marcillaud <marcimat@rezo.net>
2023-01-20 15:52:58 +0100

    build: Pour le moment ce sera une future 4.3 ici…

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -442,8 +442,8 @@ $liste_des_authentifications = [
 // pour specifier les versions de SPIP necessaires
 // il faut s'en tenir a un nombre de decimales fixe
 // ex : 2.0.0, 2.0.0-dev, 2.0.0-beta, 2.0.0-beta2
-$spip_version_branche = '4.2.0-dev';
-define('_SPIP_VERSION_ID', 40200);
+$spip_version_branche = '4.3.0-dev';
+define('_SPIP_VERSION_ID', 40300);
 define('_SPIP_EXTRA_VERSION', '-dev');
 
 // cette version dev accepte tous les plugins compatible avec la version ci-dessous
