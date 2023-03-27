0f40fc3e85cb5a2bea8f650d437738e511cb70dd
tcharlss <tcharlss@bravecassine.com>
2023-03-27 22:06:27 +0200

    fix: notices PHP en moins sur la page de contrôle des tâches de fond
    On fait un filtre pour afficher la fonction et ses arguments (crédits: 100% marcimat).
    
    Refs: #5541
    (cherry picked from commit 8c4b10318463c5f6c03819f1fac5c8a7069ffefb)

--- prive/squelettes/contenu/job_queue_fonctions.php
+++ prive/squelettes/contenu/job_queue_fonctions.php
@@ -12,3 +12,19 @@ function job_queue_block_and_watch() {
 	include_spip('inc/genie');
 	genie_queue_watch_dist();
 }
+
+/**
+ * Prévisu d'un appel à une fonction avec ses arguments
+ *
+ * @param string $function
+ * @param string $args
+ * @return string
+ */
+function job_queue_display_call(string $function, string $args): string {
+	$args = unserialize($args);
+	$args = array_map(function($arg) {
+		return is_scalar($arg) ? $arg : get_debug_type($arg);
+	}, $args);
+
+	return sprintf('%s(%s)', $function, implode(', ', $args));
+}
