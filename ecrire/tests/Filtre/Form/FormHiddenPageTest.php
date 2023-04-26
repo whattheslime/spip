726a08bd8f9cba565e4ba9a79776f877fa22cfdf
Matthieu Marcillaud <marcimat@rezo.net>
2023-04-26 21:45:45 +0200

    docs: Commentaire erroné. On l’enlève.

--- ecrire/tests/Filtre/Form/FormHiddenPageTest.php
+++ ecrire/tests/Filtre/Form/FormHiddenPageTest.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * Test unitaire de la fonction affdate_court du fichier inc/filtres.php
- */
-
 namespace Spip\Test\Filtre\Form;
 
 use PHPUnit\Framework\TestCase;
