f05a30971d5ebc4404e9ec192e3d3d8787620143
Matthieu Marcillaud <marcimat@rezo.net>
2023-07-01 12:38:34 +0200

    tests: Test balise dynamique lang en PHPUnit

--- ecrire/tests/Squelettes/Balise/data/squelettes/formulaires/test_dyn_lang.php
+++ ecrire/tests/Squelettes/Balise/data/squelettes/formulaires/test_dyn_lang.php
@@ -2,13 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * Plugin xxx
- * (c) 2009 xxx
- * Distribue sous licence GPL
- *
- */
-
 function formulaires_test_dyn_lang_charger_dist($lang_skel) {
 	return [
 		'message_ok' => $lang_skel === $GLOBALS['spip_lang'] ? 'OK' : "NOK : La langue dans le squelette appelant est {$lang_skel} mais la langue dans charger() est " . $GLOBALS['spip_lang'],
--- ecrire/tests/legacy/unit/balises/balise_dyn_langue_fonctions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// placer une langue globale arbitraire
-$GLOBALS['spip_lang'] = 'ar';
