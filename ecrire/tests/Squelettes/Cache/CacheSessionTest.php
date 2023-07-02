aa2e6d364dc88631e9e3a9ec664a9227c9271566
Matthieu Marcillaud <marcimat@rezo.net>
2023-07-02 20:28:02 +0200

    tests: Test de cache sessionné, en PHPUnit

---
+++ ecrire/tests/Squelettes/Cache/CacheSessionTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spip\Test\Squelettes\Balise;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Depends;
+use Spip\Test\SquelettesTestCase;
+
+class CacheSessionTest extends SquelettesTestCase
+{
+	private static array $errors = [];
+
+	public static function setUpBeforeClass(): void {
+		$GLOBALS['dossier_squelettes'] = self::relativePath(__DIR__ . '/data/squelettes');
+		$GLOBALS['delais'] = 3600; // See boostrap.php qui met delais = 0 (inhibe le cache)
+	}
+
+	protected function setUp(): void {
+		$this->resetErrors();
+	}
+
+	/** Vérifier qu’on sait attraper les données de cache */
+	#[DataProvider('providerVerifierCaptureMajInvalideurs')]
+	public function testVerifierCaptureMajInvalideurs(int $expectedCountErrors, string $squelette, bool $session_attendue): void {
+		$this->runWithSquelette($squelette, $session_attendue);
+		$this->assertCount($expectedCountErrors, $this->getErrors(), $this->showErrors());
+	}
+
+	public static function providerVerifierCaptureMajInvalideurs(): array {
+		return [
+			[0, 'inclure/A_session_wo', false],
+			[1, 'inclure/A_session_wo', true],
+			[2, 'inclure/A_session_w', false],
+			[0, 'inclure/A_session_w', true],
+		];
+	}
+
+	#[Depends('testVerifierCaptureMajInvalideurs')]
+	#[DataProvider('providerCachesSessionnes')]
+	public function testCachesSessionnes(int $expectedCountErrors, string $squelette, bool $session_attendue): void {
+		$this->runWithSquelette($squelette, $session_attendue);
+		$this->assertCount($expectedCountErrors, $this->getErrors(), $this->showErrors());
+	}
+
+	public static function providerCachesSessionnes(): array {
+		return [
+			[0, 'cache_session_wo_1', false],
+			[0, 'cache_session_wo_2', false],
+			[0, 'cache_session_wo_3', false],
+			[0, 'cache_session_wo_4', false],
+			[0, 'cache_session_wo_5', false],
+			[0, 'cache_session_wo_6', false],
+			[0, 'cache_session_wo_7', false],
+			[0, 'cache_session_w_1', true],
+			[0, 'cache_session_w_2', true],
+			[0, 'cache_session_w_3', true],
+		];
+	}
+
+	private function runWithSquelette(string $fond, bool $session_attendue) {
+		unset($GLOBALS['cache_utilise_session']);
+		recuperer_fond($fond, [
+			'assert_session' => $session_attendue,
+			'caller' => 'none',
+			'salt' => $this->saltContext(),
+		]);
+		unset($GLOBALS['cache_utilise_session']);
+		recuperer_fond('root', [
+			'sousfond' => $fond,
+			'inc_assert_session' => $session_attendue,
+			'salt' => $this->saltContext(),
+		]);
+	}
+
+	private function saltContext(): string {
+		return (string) time() . ':' . uniqid();
+	}
+
+	private function getErrors(): array {
+		return self::$errors;
+	}
+
+	private function showErrors(): string {
+		return "Errors:\n" . implode("\n", array_map(fn ($e) => "- $e", self::getErrors()));
+	}
+
+	private function resetErrors(): void {
+		self::$errors = [];
+	}
+
+	public static function addError(string $msg, array $page): void {
+		self::$errors[] = sprintf(
+			'%s pour %s: %s',
+			$msg,
+			$page['source'],
+			self::trace_contexte($page['contexte'])
+		);
+	}
+
+	private static function trace_contexte(array $contexte): string {
+		foreach ($contexte as $k => $v) {
+			if (str_starts_with($k, 'date_') || $k === 'salt') {
+				unset($contexte[$k]);
+			}
+		}
+		if (isset($contexte['caller']) && str_starts_with($contexte['caller'], 'tests/squelettes/')) {
+			$contexte['caller'] = substr($contexte['caller'], 17);
+		}
+
+		return json_encode($contexte, JSON_THROW_ON_ERROR);
+	}
+}
---
+++ ecrire/tests/Squelettes/Cache/data/squelettes/inc/maj_invalideurs.php
@@ -0,0 +1,26 @@
+<?php
+
+use Spip\Test\Squelettes\Balise\CacheSessionTest;
+
+/**
+ * Fonction chargée de faire les assertions d'erreur si l'invalideur session n'est pas comme on l'attend
+ *
+ * @param string $chemin_cache
+ * @param array $page
+ */
+ function inc_maj_invalideurs($chemin_cache, $page): void {
+	if (!isset($page['contexte']['assert_session'])) {
+		return;
+	}
+
+	$expected_session = (bool) $page['contexte']['assert_session'];
+	$has_session = $page['invalideurs']['session'] ?? false;
+
+	if ($expected_session) {
+		if (!$has_session) {
+			CacheSessionTest::addError('PAS de session', $page);
+		}
+	} elseif ($has_session) {
+		CacheSessionTest::addError(sprintf('SESSION %s', $has_session), $page);
+	}
+}
