219cc1bf99a44145de5a08576a30772aa0e89a96
Matthieu Marcillaud <marcimat@rezo.net>
2023-07-11 22:08:13 +0200

    chore: Fichiers de chaines de langues mères en nouveau format (pas besoin de test)

--- ecrire/lang/spip_fr.php
+++ ecrire/lang/spip_fr.php
@@ -1,9 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 // Fichier source, a modifier dans https://git.spip.net/spip/spip.git
-if (!defined('_ECRIRE_INC_VERSION')) {
-	return;
-}
 
 return [
 
