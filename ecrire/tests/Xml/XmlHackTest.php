e8c815da12d9747e1a9e6827d5163ff765849898
Matthieu Marcillaud <marcimat@rezo.net>
2023-07-11 14:46:56 +0200

    tests: tests de xml 'hack' en phpunit

---
+++ ecrire/tests/Xml/XmlHackTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spip\Test\Xml;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use Spip\Test\SquelettesTestCase;
+
+/**
+ * Le hack xml repère dans le squelette la sequence "< ?xml"
+ * et évite de l'éxecuter en php
+ */
+class XmlHackTest extends SquelettesTestCase
+{
+	#[DataProvider('providerXmlIsNotPhp')]
+	public function testXmlIsNotPhp(string $squelette): void {
+		$skel = $this->relativePath(__DIR__ . '/data/' . $squelette);
+		$out = recuperer_fond($skel, [], [
+			'raw' => true,
+			'trim' => true
+		]);
+		$this->assertEmpty($out['erreur'] ?? null);
+		$this->assertNotEmpty($out['texte']);
+		$xml = simplexml_load_string($out['texte']);
+		$this->assertOk((string) $xml[0]);
+	}
+
+	public static function providerXmlIsNotPhp(): array {
+		return [
+			['xmlhack'],
+			['xmlhack_php'],
+			['xmlhack_inclure'],
+			['xmlhack_inclure_php'],
+			['xmlhack_inclure_dyn'],
+			['xmlhack_inclure_dyn_php'],
+		];
+	}
+}
--- ecrire/tests/legacy/unit/xml/xmlhack_fonctions.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-include_spip('inc/xml');
