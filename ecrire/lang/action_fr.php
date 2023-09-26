c8c9aec730bae1f5c8746ea967caca19d6826443
tcharlss <tcharlss@bravecassine.com>
2023-09-26 18:25:37 +0200

    feat(idiomes): une chaîne de langue `action`
    Ne contient pour l'instant que des actions génériques, généralement sous la forme de verbes.
    
    `<:action:valider:>`
    `<:action:enregistrer:>`
    etc.
    
    Refs #5738

---
+++ ecrire/lang/action_fr.php
@@ -0,0 +1,78 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip/spip.git
+//
+// Chaînes de langue correspondant à des actions, typiquement pour des boutons.
+
+return [
+
+	// A
+	'accepter' => 'Accepter',
+	'ajouter' => 'Ajouter',
+	'annuler' => 'Annuler',
+	'annuler_changements' => 'Annuler les changements',
+	'associer' => 'Associer',
+
+	// C
+	'choisir' => 'Choisir',
+	'configurer' => 'Configurer',
+	'coninuer' => 'Continuer',
+	'confirmer' => 'Confirmer',
+	'connexion' => 'Connexion',
+	'creer' => 'Créer',
+
+	// D
+	'deconnexion' => 'Déconnexion',
+	'dissocier' => 'Dissocier',
+
+	// E
+	'editer' => 'Éditer',
+	'enlever' => 'Retirer',
+	'enregistrer' => 'Enregistrer',
+	'enregistrer_continuer' => 'Enregistrer et continuer',
+	'enregistrer_changements' => 'Enregistrer les changements',
+	'envoyer' => 'Envoyer',
+
+	// F
+	'fermer' => 'Fermer',
+
+	// M
+	'modifier' => 'Modifier',
+
+	// N
+	'non' => 'Non',
+
+	// O
+	'ouvrir' => 'Ouvrir',
+	'ok' => 'ok',
+	'oui' => 'Oui',
+
+	// P
+	'precedent' => 'Précédent',
+
+	// R
+	'refuser' => 'Refuser',
+	'remplacer' => 'Remplacer',
+	'retirer' => 'Retirer',
+
+	// S
+	's_inscrire' => 'S’inscrire',
+	'se_desinscrire' => 'Se désinscrire',
+	'soumettre' => 'Soumettre',
+	'suivant' => 'Suivant',
+	'supprimer' => 'Supprimer',
+
+	// S
+	'selectionner' => 'Sélectionner',
+	'sauvegarder' => 'Sauvegarder',
+	'sauvegarder_continuer' => 'Sauvegarder et continuer',
+	'sauvegarder_changements' => 'Sauvegarder les changements',
+
+	// T
+	'televerser' => 'Téléverser',
+	'telecharger' => 'Télécharger',
+
+	// V
+	'valider' => 'Valider',
+
+];
