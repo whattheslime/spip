e0bbbb7d720f6a052a94d4e958efcce05fb75bb4
JLuc <jluc@noreply.git.spip.net>
2024-01-01 22:03:26 +0100

    fix: `identifiant_slug()` peut avoir un séparateur vide
    Fix: #5810
    (cherry picked from commit a4a9375d6ef7472619d47ee50606f8457d5fed38)

--- ecrire/inc/filtres.php
+++ ecrire/inc/filtres.php
@@ -5480,13 +5480,15 @@ function identifiant_slug($texte, $type = '', $options = []) {
 	// On remplace tout ce qui n'est pas un mot par un séparateur
 	$texte = preg_replace(',[\W_]+,ms', $separateur, $texte);
 
-	// nettoyer les doubles occurences du separateur si besoin
-	while (strpos($texte, (string) "$separateur$separateur") !== false) {
-		$texte = str_replace("$separateur$separateur", $separateur, $texte);
-	}
+	if ($separateur) {
+		// nettoyer les doubles occurences du separateur si besoin
+		while (strpos($texte, (string) "$separateur$separateur") !== false) {
+			$texte = str_replace("$separateur$separateur", $separateur, $texte);
+		}
 
-	// pas de separateur au debut ni a la fin
-	$texte = trim($texte, $separateur);
+		// pas de separateur au debut ni a la fin
+		$texte = trim($texte, $separateur);
+	}
 
 	// en minuscules
 	$texte = strtolower($texte);
