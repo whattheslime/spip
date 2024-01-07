464d1e3809925c134ab58449652c2613db41e632
Matthieu Marcillaud <marcimat@rezo.net>
2024-01-07 19:14:13 +0100

    deprecate: inc_log_dist()

--- ecrire/inc/log.php
+++ ecrire/inc/log.php
@@ -1,4 +1,5 @@
 <?php
+use Psr\Log\LogLevel;
 
 /***************************************************************************\
  *  SPIP, Système de publication pour l'internet                           *
@@ -13,98 +14,32 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+/**
+ * @deprecated 5.0 Utiliser spip_logger()
+ */
 function inc_log_dist($message, $logname = null, $logdir = null, $logsuf = null) {
-	static $test_repertoire = [];
-	static $compteur = [];
-	static $debugverb = ''; // pour ne pas le recalculer au reappel
-
-	if (is_null($logname) || !is_string($logname)) {
-		$logname = defined('_FILE_LOG') ? _FILE_LOG : 'spip';
-	}
-	if (!isset($compteur[$logname])) {
-		$compteur[$logname] = 0;
-	}
-	if (
-		$logname != 'maj'
-		&& defined('_MAX_LOG')
-		&& ($compteur[$logname]++ > _MAX_LOG || !$GLOBALS['nombre_de_logs'] || !$GLOBALS['taille_des_logs'])
-	) {
-		return;
-	}
-
-	// si spip_log() est appelé dans mes_options, toutes les constantes n'ont pas été définies
-	$logfile =
-		($logdir ?? (defined('_DIR_LOG') ? _DIR_LOG : _DIR_RACINE . _NOM_TEMPORAIRES_INACCESSIBLES))
-		. $logname
-		. ($logsuf ?? (defined('_FILE_LOG_SUFFIX') ? _FILE_LOG_SUFFIX : '.log'));
-
-	if (!isset($test_repertoire[$d = dirname($logfile)])) {
-		$test_repertoire[$d] = false; // eviter une recursivite en cas d'erreur de sous_repertoire
-		$test_repertoire[$d] = (@is_dir($d) ? true : (function_exists('sous_repertoire') ? sous_repertoire(
-			$d,
-			'',
-			false,
-			true
-		) : false));
-	}
-
-	// Si le repertoire défini n'existe pas, poser dans tmp/
-	if (!$test_repertoire[$d]) {
-		$logfile = _DIR_RACINE . _NOM_TEMPORAIRES_INACCESSIBLES . $logname . '.log';
-	}
-
-	$rotate = 0;
-	$pid = '(pid ' . @getmypid() . ')';
-
-	// accepter spip_log( Array )
-	if (!is_string($message)) {
-		$message = var_export($message, true);
-	}
-
-	if (!$debugverb && defined('_LOG_FILELINE') && _LOG_FILELINE) {
-		$debug = debug_backtrace();
-		$l = $debug[1]['line'];
-		$fi = $debug[1]['file'];
-		if (str_starts_with($fi, _ROOT_RACINE)) {
-			$fi = substr($fi, strlen(_ROOT_RACINE));
-		}
-		$fu = $debug[2]['function'] ?? '';
-		$debugverb = "$fi:L$l:$fu" . '():';
-	}
-
-	$m = date('Y-m-d H:i:s') . ' ' . ($GLOBALS['ip'] ?? '') . ' ' . $pid . ' '
-		//distinguer les logs prives et publics dans les grep
-		. $debugverb
-		. (test_espace_prive() ? ':Pri:' : ':Pub:')
-		. preg_replace("/\n*$/", "\n", $message);
-
-
-	if (
-		@is_readable($logfile)
-		&& ((!$s = @filesize($logfile)) || $s > $GLOBALS['taille_des_logs'] * 1024)
-	) {
-		$rotate = $GLOBALS['nombre_de_logs'];
-		$m .= "[-- rotate --]\n";
-	}
-
-	$f = @fopen($logfile, 'ab');
-	if ($f) {
-		fwrite($f, (defined('_LOG_BRUT') && _LOG_BRUT) ? $m : str_replace('<', '&lt;', $m));
-		fclose($f);
-	}
-
-	if (
-		$rotate-- > 0 && function_exists('spip_unlink')
-	) {
-		spip_unlink($logfile . '.' . $rotate);
-		while ($rotate--) {
-			@rename($logfile . ($rotate ? '.' . $rotate : ''), $logfile . '.' . ($rotate + 1));
+	trigger_deprecation(
+		'spip',
+		'5.0',
+		'La fonction inc_log_dist() est déprécée.' .
+			' Utilisez spip_logger() à la place.'
+	);
+	static $pre = [
+		'HS:' => LogLevel::EMERGENCY,
+		'ALERTE:' => LogLevel::ALERT,
+		'CRITIQUE:' => LogLevel::CRITICAL,
+		'ERREUR:' => LogLevel::ERROR,
+		'WARNING:' => LogLevel::WARNING,
+		'!INFO:' => LogLevel::NOTICE,
+		'info:' => LogLevel::INFO,
+		'debug:' => LogLevel::DEBUG,
+	];
+	foreach ($pre as $start => $level) {
+		if (str_starts_with($message, $start)) {
+			spip_logger($logname)->log($level, ltrim(substr($message, strlen($start))));
+			return;
 		}
 	}
-
-	// Dupliquer les erreurs specifiques dans le log general
-	if (defined('_FILE_LOG') && $logname !== _FILE_LOG) {
-		inc_log_dist($logname == 'maj' ? 'cf maj.log' : $message);
-	}
-	$debugverb = '';
+	spip_logger($logname)->info($message);
+	return;
 }
