6d43f1e0b25eeab3b1c7db83a0179afb64843f9a
b_b <bruno@eliaz.fr>
2024-07-23 18:03:14 +0200

    chore: passer ce log en debug au lieu d'info
    Fix: #5879

--- ecrire/bootstrap/inc/path.php
+++ ecrire/bootstrap/inc/path.php
@@ -180,7 +180,7 @@ function find_in_theme($file, $subdir = '', $include = false) {
 			return $themefiles["$subdir$file"] = $f;
 		}
 	}
-	spip_logger('theme')->info("$file introuvable dans le theme prive " . reset($themes));
+	spip_logger('theme')->debug("$file introuvable dans le theme prive " . reset($themes));
 
 	return $themefiles["$subdir$file"] = '';
 }
