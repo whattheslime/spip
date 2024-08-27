59520da81dd2e41d8974c98cff06d4a5c1032307
cpol0 <cpol0@pol-carre.fr>
2024-08-27 16:56:50 +0200

    fix: Servir des caches à jour pour les bots sur des redirections 301
    Ajout d'une meta `cache_bot_invalide` pour sauvegarder la liste des
    articles nécesitant un cache à jour pour les bots.
    
    Fix: #5973
    (cherry picked from commit dd716b6f3ccd941c5bc101d107ef241e04c751d0)

--- ecrire/action/purger.php
+++ ecrire/action/purger.php
@@ -73,6 +73,7 @@ function action_purger_dist($arg = null) {
 			purger_repertoire(_DIR_AIDE);
 			purger_repertoire(_DIR_VAR . 'cache-css');
 			purger_repertoire(_DIR_VAR . 'cache-js');
+			effacer_meta('cache_bot_invalide');
 			break;
 
 		case 'squelettes':
--- ecrire/public/cacher.php
+++ ecrire/public/cacher.php
@@ -214,8 +214,8 @@ function cache_valide(&$page, $date) {
 	}
 
 	// Sinon comparer l'age du fichier a sa duree de cache
-	$duree = intval($page['entetes']['X-Spip-Cache']);
-	$cache_mark = ($GLOBALS['meta']['cache_mark'] ?? 0);
+	$duree = (int) $page['entetes']['X-Spip-Cache'];
+	$cache_mark = max($GLOBALS['meta']['cache_mark'] ?? 0, $GLOBALS['meta']['cache_bot_invalide'] ?? 0);
 	if ($duree == 0) {  #CACHE{0}
 	return -1;
 	} // sauf pour les bots, qui utilisent toujours le cache
