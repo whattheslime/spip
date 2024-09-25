ddf6bd49f98f63ba5b4688eda89bf2f6ce936328
Cerdic <cedric@yterium.com>
2024-09-25 10:53:20 +0200

    fix: il faut vider les buffer avant de livrer le fichier sinon ça ne marche parfois pas
    (et attention à ini_set qu'on ne peut pas toujours utiliser)

--- ecrire/inc/livrer_fichier.php
+++ ecrire/inc/livrer_fichier.php
@@ -48,6 +48,18 @@ function spip_livrer_fichier($fichier, $content_type = 'application/octet-stream
 		$options['range'] = $_SERVER['HTTP_RANGE'];
 	}
 
+	// vider les buffer et supprimer la compression si besoin
+	if (function_exists('ini_set')) {
+		@ini_set('zlib.output_compression', '0'); // pour permettre l'affichage au fur et a mesure
+		@ini_set('output_buffering', 'off');
+		@ini_set('implicit_flush', 1);
+	}
+	@ob_implicit_flush(true);
+	$level = ob_get_level();
+	while ($level--) {
+		@ob_end_clean();
+	}
+
 	spip_livrer_fichier_entetes(
 		$fichier,
 		$content_type,
