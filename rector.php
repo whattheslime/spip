0316d304913f5455114a45d1fb5b031a3e4ff8d9
Matthieu Marcillaud <marcimat@rezo.net>
2024-09-26 11:59:48 +0200

    chore: up config rector

--- rector.php
+++ rector.php
@@ -2,24 +2,27 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\LogicalAnd\LogicalToBooleanRector;
 use Rector\Config\RectorConfig;
 use Rector\Php71\Rector\FuncCall\CountOnNullRector;
 use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
 use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-	$rectorConfig->paths([__DIR__ . '/ecrire', __DIR__ . '/prive', __DIR__ . '/spip.php', __DIR__ . '/index.php']);
 
-	$rectorConfig->sets([LevelSetList::UP_TO_PHP_81, LevelSetList::UP_TO_PHP_82]);
-
-	$rectorConfig->skip([
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/ecrire',
+	])
+	->withRootFiles()
+	->withPhpSets(php82: true)
+	#->withPreparedSets(deadCode: true, codeQuality: true)
+	->withRules([LogicalToBooleanRector::class])
+	->withSkip(
 		__DIR__ . '/ecrire/lang',
 		FinalizePublicClassConstantRector::class,
 		NullToStrictStringFuncCallArgRector::class,
 		CountOnNullRector::class,
 		MixedTypeRector::class,
-	]);
-
-};
+	)
+;
