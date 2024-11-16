9da7ce979bd83c0e09d267243c027effaf160c2c
JamesRezo <james@rezo.net>
2024-11-17 00:27:41 +0100

    feat(5.x): externalisation du dossier ecrire/. spip/spip devient un "project" composer

--- ecrire/tests/Squelettes/Balise/data/squelettes/formulaires/test_dyn_lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-function formulaires_test_dyn_lang_charger_dist($lang_skel) {
-	return [
-		'message_ok' => $lang_skel === $GLOBALS['spip_lang'] ? 'OK' : "NOK : La langue dans le squelette appelant est {$lang_skel} mais la langue dans charger() est " . $GLOBALS['spip_lang'],
-	];
-}
